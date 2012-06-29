7821f5d9c22a6d9c1ac2d5415715784e08fa8075
Scott Killen <scott@scottkillen.com>
2012-06-29 02:56:44 -0400

    Fixed custom grass so that its blocks will be repleaceable

--- extrabiomes/src/minecraft/extrabiomes/blocks/BlockCustomTallGrass.java
+++ extrabiomes/src/minecraft/extrabiomes/blocks/BlockCustomTallGrass.java
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) Scott Killen and MisterFiber, 2012
+ * 
+ * This mod is distributed under the terms of the Minecraft Mod Public
+ * License 1.0, or MMPL. Please check the contents of the license
+ * located in /MMPL-1.0.txt
+ */
+
 package extrabiomes.blocks;
 
 import java.util.ArrayList;
@@ -16,20 +24,22 @@ import net.minecraft.src.forge.ITextureProvider;
 import extrabiomes.ColorizerExtraBiomes;
 import extrabiomes.api.TerrainGenManager;
 
-public class BlockCustomTallGrass extends BlockFlower implements IShearable,
-		ITextureProvider {
+public class BlockCustomTallGrass extends BlockFlower implements
+		IShearable, ITextureProvider
+{
 
-	public static final int metaBrown = 0;
-	public static final int metaShortBrown = 1;
-	public static final int metaDead = 2;
-	public static final int metaDeadTall = 3;
-	public static final int metaDeadYellow = 4;
+	public static final int	metaBrown		= 0;
+
+	public static final int	metaShortBrown	= 1;
+	public static final int	metaDead		= 2;
+	public static final int	metaDeadTall	= 3;
+	public static final int	metaDeadYellow	= 4;
 
 	public BlockCustomTallGrass(int id) {
 		super(id, 48, Material.vine);
-		float var3 = 0.4F;
-		setBlockBounds(0.5F - var3, 0.0F, 0.5F - var3, 0.5F + var3, 0.8F,
-				0.5F + var3);
+		final float var3 = 0.4F;
+		setBlockBounds(0.5F - var3, 0.0F, 0.5F - var3, 0.5F + var3,
+				0.8F, 0.5F + var3);
 		setHardness(0F);
 		setStepSound(soundGrassFootstep);
 	}
@@ -45,10 +55,11 @@ public class BlockCustomTallGrass extends BlockFlower implements IShearable,
 
 	@Override
 	public boolean canBlockStay(World world, int x, int y, int z) {
-		int metaGrass = world.getBlockMetadata(x, y, z);
-		int blockUnder = world.getBlockId(x, y - 1, z);
+		final int metaGrass = world.getBlockMetadata(x, y, z);
+		final int blockUnder = world.getBlockId(x, y - 1, z);
 
-		return ((metaGrass == metaBrown || metaGrass == metaShortBrown) && blockUnder == TerrainGenManager.blockMountainRidge.blockID)
+		return (metaGrass == metaBrown || metaGrass == metaShortBrown)
+				&& blockUnder == TerrainGenManager.blockMountainRidge.blockID
 				|| (metaGrass == metaDead || metaGrass == metaDeadTall || metaGrass == metaDeadYellow)
 				&& blockUnder == TerrainGenManager.blockWasteland.blockID
 				|| super.canBlockStay(world, x, y, z);
@@ -56,7 +67,7 @@ public class BlockCustomTallGrass extends BlockFlower implements IShearable,
 
 	@Override
 	public boolean canPlaceBlockAt(World world, int x, int y, int z) {
-		int blockUnder = world.getBlockId(x, y, z);
+		final int blockUnder = world.getBlockId(x, y, z);
 		return blockUnder == TerrainGenManager.blockMountainRidge.blockID
 				|| blockUnder == TerrainGenManager.blockWasteland.blockID
 				|| super.canPlaceBlockAt(world, x, y, z);
@@ -70,7 +81,9 @@ public class BlockCustomTallGrass extends BlockFlower implements IShearable,
 	}
 
 	@Override
-	public int colorMultiplier(IBlockAccess iBlockAccess, int x, int y, int z) {
+	public int colorMultiplier(IBlockAccess iBlockAccess, int x, int y,
+			int z)
+	{
 		return ColorizerExtraBiomes.getNonBiomeColor();
 	}
 
@@ -82,27 +95,26 @@ public class BlockCustomTallGrass extends BlockFlower implements IShearable,
 	}
 
 	@Override
-	public ArrayList<ItemStack> getBlockDropped(World world, int x, int y,
-			int z, int metadata, int fortune) {
-		ArrayList<ItemStack> ret = new ArrayList<ItemStack>();
+	public ArrayList<ItemStack> getBlockDropped(World world, int x,
+			int y, int z, int metadata, int fortune)
+	{
+		final ArrayList<ItemStack> ret = new ArrayList<ItemStack>();
 		int rarity = 8;
-		if (metadata == BlockCustomTallGrass.metaDead || metadata == BlockCustomTallGrass.metaDeadTall
+		if (metadata == BlockCustomTallGrass.metaDead
+				|| metadata == BlockCustomTallGrass.metaDeadTall
 				|| metadata == BlockCustomTallGrass.metaDeadYellow)
 			rarity *= 2;
-		if (world.rand.nextInt(rarity) != 0) {
-			return ret;
-		}
+		if (world.rand.nextInt(rarity) != 0) return ret;
 
-		ItemStack item = ForgeHooks.getGrassSeed(world);
+		final ItemStack item = ForgeHooks.getGrassSeed(world);
 
-		if (item != null) {
-			ret.add(item);
-		}
+		if (item != null) ret.add(item);
 		return ret;
 	}
 
 	@Override
-	public int getBlockTextureFromSideAndMetadata(int side, int metadata) {
+	public int getBlockTextureFromSideAndMetadata(int side, int metadata)
+	{
 		return super.getBlockTextureFromSideAndMetadata(side, metadata)
 				+ metadata;
 	}
@@ -118,8 +130,9 @@ public class BlockCustomTallGrass extends BlockFlower implements IShearable,
 	}
 
 	@Override
-	public void harvestBlock(World world, EntityPlayer player, int x, int y,
-			int z, int metadata) {
+	public void harvestBlock(World world, EntityPlayer player, int x,
+			int y, int z, int metadata)
+	{
 		super.harvestBlock(world, player, x, y, z, metadata);
 	}
 
@@ -129,14 +142,23 @@ public class BlockCustomTallGrass extends BlockFlower implements IShearable,
 	}
 
 	@Override
-	public boolean isShearable(ItemStack item, World world, int x, int y, int z) {
+	public boolean isBlockReplaceable(World world, int x, int y, int z)
+	{
 		return true;
 	}
 
 	@Override
-	public ArrayList<ItemStack> onSheared(ItemStack item, World world, int x,
-			int y, int z, int fortune) {
-		ArrayList<ItemStack> ret = new ArrayList<ItemStack>();
+	public boolean isShearable(ItemStack item, World world, int x,
+			int y, int z)
+	{
+		return true;
+	}
+
+	@Override
+	public ArrayList<ItemStack> onSheared(ItemStack item, World world,
+			int x, int y, int z, int fortune)
+	{
+		final ArrayList<ItemStack> ret = new ArrayList<ItemStack>();
 		ret.add(new ItemStack(this, 1, world.getBlockMetadata(x, y, z)));
 		return ret;
 	}
--- extrabiomes/src/minecraft_server/extrabiomes/blocks/BlockCustomTallGrass.java
+++ extrabiomes/src/minecraft_server/extrabiomes/blocks/BlockCustomTallGrass.java
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) Scott Killen and MisterFiber, 2012
+ * 
+ * This mod is distributed under the terms of the Minecraft Mod Public
+ * License 1.0, or MMPL. Please check the contents of the license
+ * located in /MMPL-1.0.txt
+ */
+
 package extrabiomes.blocks;
 
 import java.util.ArrayList;
@@ -5,7 +13,6 @@ import java.util.Random;
 
 import net.minecraft.src.BlockFlower;
 import net.minecraft.src.EntityPlayer;
-import net.minecraft.src.IBlockAccess;
 import net.minecraft.src.ItemStack;
 import net.minecraft.src.Material;
 import net.minecraft.src.World;
@@ -14,20 +21,22 @@ import net.minecraft.src.forge.IShearable;
 import net.minecraft.src.forge.ITextureProvider;
 import extrabiomes.api.TerrainGenManager;
 
-public class BlockCustomTallGrass extends BlockFlower implements IShearable,
-		ITextureProvider {
+public class BlockCustomTallGrass extends BlockFlower implements
+		IShearable, ITextureProvider
+{
 
-	public static final int metaBrown = 0;
-	public static final int metaShortBrown = 1;
-	public static final int metaDead = 2;
-	public static final int metaDeadTall = 3;
-	public static final int metaDeadYellow = 4;
+	public static final int	metaBrown		= 0;
+
+	public static final int	metaShortBrown	= 1;
+	public static final int	metaDead		= 2;
+	public static final int	metaDeadTall	= 3;
+	public static final int	metaDeadYellow	= 4;
 
 	public BlockCustomTallGrass(int id) {
 		super(id, 48, Material.vine);
-		float var3 = 0.4F;
-		setBlockBounds(0.5F - var3, 0.0F, 0.5F - var3, 0.5F + var3, 0.8F,
-				0.5F + var3);
+		final float var3 = 0.4F;
+		setBlockBounds(0.5F - var3, 0.0F, 0.5F - var3, 0.5F + var3,
+				0.8F, 0.5F + var3);
 		setHardness(0F);
 		setStepSound(soundGrassFootstep);
 	}
@@ -43,10 +52,11 @@ public class BlockCustomTallGrass extends BlockFlower implements IShearable,
 
 	@Override
 	public boolean canBlockStay(World world, int x, int y, int z) {
-		int metaGrass = world.getBlockMetadata(x, y, z);
-		int blockUnder = world.getBlockId(x, y - 1, z);
+		final int metaGrass = world.getBlockMetadata(x, y, z);
+		final int blockUnder = world.getBlockId(x, y - 1, z);
 
-		return ((metaGrass == metaBrown || metaGrass == metaShortBrown) && blockUnder == TerrainGenManager.blockMountainRidge.blockID)
+		return (metaGrass == metaBrown || metaGrass == metaShortBrown)
+				&& blockUnder == TerrainGenManager.blockMountainRidge.blockID
 				|| (metaGrass == metaDead || metaGrass == metaDeadTall || metaGrass == metaDeadYellow)
 				&& blockUnder == TerrainGenManager.blockWasteland.blockID
 				|| super.canBlockStay(world, x, y, z);
@@ -54,7 +64,7 @@ public class BlockCustomTallGrass extends BlockFlower implements IShearable,
 
 	@Override
 	public boolean canPlaceBlockAt(World world, int x, int y, int z) {
-		int blockUnder = world.getBlockId(x, y, z);
+		final int blockUnder = world.getBlockId(x, y, z);
 		return blockUnder == TerrainGenManager.blockMountainRidge.blockID
 				|| blockUnder == TerrainGenManager.blockWasteland.blockID
 				|| super.canPlaceBlockAt(world, x, y, z);
@@ -68,27 +78,26 @@ public class BlockCustomTallGrass extends BlockFlower implements IShearable,
 	}
 
 	@Override
-	public ArrayList<ItemStack> getBlockDropped(World world, int x, int y,
-			int z, int metadata, int fortune) {
-		ArrayList<ItemStack> ret = new ArrayList<ItemStack>();
+	public ArrayList<ItemStack> getBlockDropped(World world, int x,
+			int y, int z, int metadata, int fortune)
+	{
+		final ArrayList<ItemStack> ret = new ArrayList<ItemStack>();
 		int rarity = 8;
-		if (metadata == BlockCustomTallGrass.metaDead || metadata == BlockCustomTallGrass.metaDeadTall
+		if (metadata == BlockCustomTallGrass.metaDead
+				|| metadata == BlockCustomTallGrass.metaDeadTall
 				|| metadata == BlockCustomTallGrass.metaDeadYellow)
 			rarity *= 2;
-		if (world.rand.nextInt(rarity) != 0) {
-			return ret;
-		}
+		if (world.rand.nextInt(rarity) != 0) return ret;
 
-		ItemStack item = ForgeHooks.getGrassSeed(world);
+		final ItemStack item = ForgeHooks.getGrassSeed(world);
 
-		if (item != null) {
-			ret.add(item);
-		}
+		if (item != null) ret.add(item);
 		return ret;
 	}
 
 	@Override
-	public int getBlockTextureFromSideAndMetadata(int side, int metadata) {
+	public int getBlockTextureFromSideAndMetadata(int side, int metadata)
+	{
 		return super.getBlockTextureFromSideAndMetadata(side, metadata)
 				+ metadata;
 	}
@@ -99,8 +108,9 @@ public class BlockCustomTallGrass extends BlockFlower implements IShearable,
 	}
 
 	@Override
-	public void harvestBlock(World world, EntityPlayer player, int x, int y,
-			int z, int metadata) {
+	public void harvestBlock(World world, EntityPlayer player, int x,
+			int y, int z, int metadata)
+	{
 		super.harvestBlock(world, player, x, y, z, metadata);
 	}
 
@@ -110,14 +120,23 @@ public class BlockCustomTallGrass extends BlockFlower implements IShearable,
 	}
 
 	@Override
-	public boolean isShearable(ItemStack item, World world, int x, int y, int z) {
+	public boolean isBlockReplaceable(World world, int x, int y, int z)
+	{
 		return true;
 	}
 
 	@Override
-	public ArrayList<ItemStack> onSheared(ItemStack item, World world, int x,
-			int y, int z, int fortune) {
-		ArrayList<ItemStack> ret = new ArrayList<ItemStack>();
+	public boolean isShearable(ItemStack item, World world, int x,
+			int y, int z)
+	{
+		return true;
+	}
+
+	@Override
+	public ArrayList<ItemStack> onSheared(ItemStack item, World world,
+			int x, int y, int z, int fortune)
+	{
+		final ArrayList<ItemStack> ret = new ArrayList<ItemStack>();
 		ret.add(new ItemStack(this, 1, world.getBlockMetadata(x, y, z)));
 		return ret;
 	}
