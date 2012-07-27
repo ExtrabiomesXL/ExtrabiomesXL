02e164f4492e7a8adc4954e94b177ae94abfa3e7
Scott Killen <scott@scottkillen.com>
2012-07-27 18:32:48 -0400

    Updated forestry API

--- extrabiomes/src/common/extrabiomes/plugins/PluginForestry.java
+++ extrabiomes/src/common/extrabiomes/plugins/PluginForestry.java
@@ -1,3 +1,4 @@
+
 package extrabiomes.plugins;
 
 import java.util.Collection;
@@ -6,16 +7,6 @@ import net.minecraft.src.BiomeGenBase;
 import net.minecraft.src.Block;
 import net.minecraft.src.ItemStack;
 import net.minecraft.src.ModLoader;
-import forestry.api.apiculture.FlowerManager;
-import forestry.api.core.EnumHumidity;
-import forestry.api.core.EnumTemperature;
-import forestry.api.core.ForestryBlock;
-import forestry.api.core.GlobalManager;
-import forestry.api.core.ItemInterface;
-import forestry.api.cultivation.CropProviders;
-import forestry.api.liquids.LiquidStack;
-import forestry.api.storage.BackpackManager;
-import forestry.api.recipes.RecipeManagers;
 import extrabiomes.api.BiomeDecorationsManager;
 import extrabiomes.api.ExtrabiomesBlock;
 import extrabiomes.api.IBiomeDecoration;
@@ -30,16 +21,26 @@ import extrabiomes.blocks.BlockGreenLeaves;
 import extrabiomes.plugins.forestry.CropProviderSapling;
 import extrabiomes.plugins.forestry.WorldGenChunkBogEarth;
 import extrabiomes.terrain.BiomeDecoration;
+import forestry.api.apiculture.FlowerManager;
+import forestry.api.core.EnumHumidity;
+import forestry.api.core.EnumTemperature;
+import forestry.api.core.ForestryBlock;
+import forestry.api.core.GlobalManager;
+import forestry.api.core.ItemInterface;
+import forestry.api.cultivation.CropProviders;
+import forestry.api.liquids.LiquidStack;
+import forestry.api.recipes.RecipeManagers;
+import forestry.api.storage.BackpackManager;
 
 public enum PluginForestry implements IPlugin {
 	INSTANCE;
 
-	private static final int DIGGER = 1;
-	private static final int FORESTER = 2;
-	private static final int ADVENTURER = 4;
-	private static ItemStack liquidJuice;
-	private static ItemStack liquidHoney;
-	private static ItemStack liquidBiomass;
+	private static final int	DIGGER		= 1;
+	private static final int	FORESTER	= 2;
+	private static final int	ADVENTURER	= 4;
+	private static ItemStack	liquidJuice;
+	private static ItemStack	liquidHoney;
+	private static ItemStack	liquidBiomass;
 
 	private static void addBackpackItems() {
 		if (ExtrabiomesBlock.crackedSand != null)
@@ -50,8 +51,8 @@ public enum PluginForestry implements IPlugin {
 					ExtrabiomesBlock.redRock));
 
 		if (ExtrabiomesBlock.quickSand != null)
-			BackpackManager.backpackItems[ADVENTURER].add(new ItemStack(
-					ExtrabiomesBlock.quickSand));
+			BackpackManager.backpackItems[ADVENTURER]
+					.add(new ItemStack(ExtrabiomesBlock.quickSand));
 
 		if (ExtrabiomesBlock.autumnLeaves != null) {
 			BackpackManager.backpackItems[FORESTER].add(new ItemStack(
@@ -69,7 +70,8 @@ public enum PluginForestry implements IPlugin {
 		}
 		if (ExtrabiomesBlock.greenLeaves != null) {
 			BackpackManager.backpackItems[FORESTER].add(new ItemStack(
-					ExtrabiomesBlock.greenLeaves, 1, BlockGreenLeaves.metaFir));
+					ExtrabiomesBlock.greenLeaves, 1,
+					BlockGreenLeaves.metaFir));
 			BackpackManager.backpackItems[FORESTER].add(new ItemStack(
 					ExtrabiomesBlock.greenLeaves, 1,
 					BlockGreenLeaves.metaRedwood));
@@ -80,99 +82,138 @@ public enum PluginForestry implements IPlugin {
 
 		if (ExtrabiomesBlock.sapling != null) {
 			BackpackManager.backpackItems[FORESTER].add(new ItemStack(
-					ExtrabiomesBlock.sapling, 1, BlockCustomSapling.metaBrown));
-			BackpackManager.backpackItems[FORESTER]
-					.add(new ItemStack(ExtrabiomesBlock.sapling, 1,
-							BlockCustomSapling.metaOrange));
-			BackpackManager.backpackItems[FORESTER]
-					.add(new ItemStack(ExtrabiomesBlock.sapling, 1,
-							BlockCustomSapling.metaPurple));
-			BackpackManager.backpackItems[FORESTER]
-					.add(new ItemStack(ExtrabiomesBlock.sapling, 1,
-							BlockCustomSapling.metaYellow));
+					ExtrabiomesBlock.sapling, 1,
+					BlockCustomSapling.metaBrown));
 			BackpackManager.backpackItems[FORESTER].add(new ItemStack(
-					ExtrabiomesBlock.sapling, 1, BlockCustomSapling.metaFir));
-			BackpackManager.backpackItems[FORESTER]
-					.add(new ItemStack(ExtrabiomesBlock.sapling, 1,
-							BlockCustomSapling.metaRedWood));
-			BackpackManager.backpackItems[FORESTER]
-					.add(new ItemStack(ExtrabiomesBlock.sapling, 1,
-							BlockCustomSapling.metaAcacia));
+					ExtrabiomesBlock.sapling, 1,
+					BlockCustomSapling.metaOrange));
+			BackpackManager.backpackItems[FORESTER].add(new ItemStack(
+					ExtrabiomesBlock.sapling, 1,
+					BlockCustomSapling.metaPurple));
+			BackpackManager.backpackItems[FORESTER].add(new ItemStack(
+					ExtrabiomesBlock.sapling, 1,
+					BlockCustomSapling.metaYellow));
+			BackpackManager.backpackItems[FORESTER].add(new ItemStack(
+					ExtrabiomesBlock.sapling, 1,
+					BlockCustomSapling.metaFir));
+			BackpackManager.backpackItems[FORESTER].add(new ItemStack(
+					ExtrabiomesBlock.sapling, 1,
+					BlockCustomSapling.metaRedWood));
+			BackpackManager.backpackItems[FORESTER].add(new ItemStack(
+					ExtrabiomesBlock.sapling, 1,
+					BlockCustomSapling.metaAcacia));
 		}
 
 		if (ExtrabiomesBlock.flower != null) {
 			BackpackManager.backpackItems[FORESTER].add(new ItemStack(
 					ExtrabiomesBlock.flower, 1,
 					BlockCustomFlower.metaAutumnShrub));
-			BackpackManager.backpackItems[FORESTER]
-					.add(new ItemStack(ExtrabiomesBlock.flower, 1,
-							BlockCustomFlower.metaHydrangea));
 			BackpackManager.backpackItems[FORESTER].add(new ItemStack(
-					ExtrabiomesBlock.flower, 1, BlockCustomFlower.metaOrange));
+					ExtrabiomesBlock.flower, 1,
+					BlockCustomFlower.metaHydrangea));
 			BackpackManager.backpackItems[FORESTER].add(new ItemStack(
-					ExtrabiomesBlock.flower, 1, BlockCustomFlower.metaPurple));
+					ExtrabiomesBlock.flower, 1,
+					BlockCustomFlower.metaOrange));
 			BackpackManager.backpackItems[FORESTER].add(new ItemStack(
-					ExtrabiomesBlock.flower, 1, BlockCustomFlower.metaRoot));
+					ExtrabiomesBlock.flower, 1,
+					BlockCustomFlower.metaPurple));
+			BackpackManager.backpackItems[FORESTER].add(new ItemStack(
+					ExtrabiomesBlock.flower, 1,
+					BlockCustomFlower.metaRoot));
 			BackpackManager.backpackItems[FORESTER].add(new ItemStack(
 					ExtrabiomesBlock.flower, 1,
 					BlockCustomFlower.metaTinyCactus));
-			BackpackManager.backpackItems[FORESTER]
-					.add(new ItemStack(ExtrabiomesBlock.flower, 1,
-							BlockCustomFlower.metaToadstool));
 			BackpackManager.backpackItems[FORESTER].add(new ItemStack(
-					ExtrabiomesBlock.flower, 1, BlockCustomFlower.metaWhite));
+					ExtrabiomesBlock.flower, 1,
+					BlockCustomFlower.metaToadstool));
+			BackpackManager.backpackItems[FORESTER].add(new ItemStack(
+					ExtrabiomesBlock.flower, 1,
+					BlockCustomFlower.metaWhite));
 		}
 	}
 
 	private static void addBasicFlowers() {
-		if (ExtrabiomesBlock.flower == null)
-			return;
+		if (ExtrabiomesBlock.flower == null) return;
 
-		FlowerManager.plainFlowers.add(new ItemStack(ExtrabiomesBlock.flower,
-				1, BlockCustomFlower.metaHydrangea));
-		FlowerManager.plainFlowers.add(new ItemStack(ExtrabiomesBlock.flower,
-				1, BlockCustomFlower.metaOrange));
-		FlowerManager.plainFlowers.add(new ItemStack(ExtrabiomesBlock.flower,
-				1, BlockCustomFlower.metaPurple));
-		FlowerManager.plainFlowers.add(new ItemStack(ExtrabiomesBlock.flower,
-				1, BlockCustomFlower.metaWhite));
+		FlowerManager.plainFlowers.add(new ItemStack(
+				ExtrabiomesBlock.flower, 1,
+				BlockCustomFlower.metaHydrangea));
+		FlowerManager.plainFlowers.add(new ItemStack(
+				ExtrabiomesBlock.flower, 1,
+				BlockCustomFlower.metaOrange));
+		FlowerManager.plainFlowers.add(new ItemStack(
+				ExtrabiomesBlock.flower, 1,
+				BlockCustomFlower.metaPurple));
+		FlowerManager.plainFlowers
+				.add(new ItemStack(ExtrabiomesBlock.flower, 1,
+						BlockCustomFlower.metaWhite));
 	}
 
 	private static void addBogEarthToGreenSwampBiome() {
-		BiomeDecoration bogEarth = new BiomeDecoration(1,
+		final BiomeDecoration bogEarth = new BiomeDecoration(1,
 				new WorldGenChunkBogEarth());
-		Collection<IBiomeDecoration> greenSwampDecorations = BiomeDecorationsManager.biomeDecorations
+		final Collection<IBiomeDecoration> greenSwampDecorations = BiomeDecorationsManager.biomeDecorations
 				.get(CustomBiomeManager.greenSwamp);
 		greenSwampDecorations.add(bogEarth);
 	}
 
 	private static void addClimates() {
-		for (BiomeGenBase biome : ExtrabiomeGenBase.customBiomeList) {
-			if (biome == null) {
-				continue;
-			}
-			if (biome.temperature >= 2.0F) {
-				EnumTemperature.hotBiomeIds.add(Integer.valueOf(biome.biomeID));
-			} else if (biome.temperature >= 1.2F)
-				EnumTemperature.warmBiomeIds
-						.add(Integer.valueOf(biome.biomeID));
+		for (final BiomeGenBase biome : ExtrabiomeGenBase.customBiomeList)
+		{
+			if (biome == null) continue;
+			if (biome.temperature >= 2.0F)
+				EnumTemperature.hotBiomeIds.add(Integer
+						.valueOf(biome.biomeID));
+			else if (biome.temperature >= 1.2F)
+				EnumTemperature.warmBiomeIds.add(Integer
+						.valueOf(biome.biomeID));
 			else if (biome.temperature >= 0.2F)
 				EnumTemperature.normalBiomeIds.add(Integer
 						.valueOf(biome.biomeID));
 			else if (biome.temperature >= 0.15F)
-				EnumTemperature.coldBiomeIds
-						.add(Integer.valueOf(biome.biomeID));
-			else {
-				EnumTemperature.icyBiomeIds.add(Integer.valueOf(biome.biomeID));
-			}
+				EnumTemperature.coldBiomeIds.add(Integer
+						.valueOf(biome.biomeID));
+			else
+				EnumTemperature.icyBiomeIds.add(Integer
+						.valueOf(biome.biomeID));
 
 			if (biome.rainfall >= 0.9F)
-				EnumHumidity.dampBiomeIds.add(Integer.valueOf(biome.biomeID));
+				EnumHumidity.dampBiomeIds.add(Integer
+						.valueOf(biome.biomeID));
 			else if (biome.rainfall >= 0.3F)
-				EnumHumidity.normalBiomeIds.add(Integer.valueOf(biome.biomeID));
+				EnumHumidity.normalBiomeIds.add(Integer
+						.valueOf(biome.biomeID));
 			else
-				EnumHumidity.aridBiomeIds.add(Integer.valueOf(biome.biomeID));
+				EnumHumidity.aridBiomeIds.add(Integer
+						.valueOf(biome.biomeID));
 		}
+	}
+
+	private static void addFermenterRecipeSapling(ItemStack resource) {
+		RecipeManagers.fermenterManager.addRecipe(
+				resource,
+				800,
+				1.0f,
+				new LiquidStack(liquidBiomass.itemID, 1, liquidBiomass
+						.getItemDamage()), new LiquidStack(
+						Block.waterStill, 1));
+		RecipeManagers.fermenterManager.addRecipe(
+				resource,
+				800,
+				1.5f,
+				new LiquidStack(liquidBiomass.itemID, 1, liquidBiomass
+						.getItemDamage()),
+				new LiquidStack(liquidJuice.itemID, 1, liquidJuice
+						.getItemDamage()));
+		RecipeManagers.fermenterManager.addRecipe(
+				resource,
+				800,
+				1.5f,
+				new LiquidStack(liquidBiomass.itemID, 1, liquidBiomass
+						.getItemDamage()),
+				new LiquidStack(liquidHoney.itemID, 1, liquidHoney
+						.getItemDamage()));
+
 	}
 
 	private static void addGlobals() {
@@ -183,35 +224,39 @@ public enum PluginForestry implements IPlugin {
 			GlobalManager.leafBlockIds
 					.add(ExtrabiomesBlock.greenLeaves.blockID);
 	}
-	private static void addFermenterRecipeSapling(ItemStack resource) {
-		RecipeManagers.fermenterManager.addRecipe(resource, 800, 1.0f, new LiquidStack(liquidBiomass.itemID, 1, liquidBiomass.getItemDamage()),
-				new LiquidStack(Block.waterStill, 1));
-		RecipeManagers.fermenterManager.addRecipe(resource, 800, 1.5f, new LiquidStack(liquidBiomass.itemID, 1, liquidBiomass.getItemDamage()),
-				new LiquidStack(liquidJuice.itemID, 1, liquidJuice.getItemDamage()));
-		RecipeManagers.fermenterManager.addRecipe(resource, 800, 1.5f, new LiquidStack(liquidBiomass.itemID, 1, liquidBiomass.getItemDamage()),
-				new LiquidStack(liquidHoney.itemID, 1, liquidHoney.getItemDamage()));
-		
-	}
+
 	private static void addRecipes() {
-		if (RecipeManagers.fermenterManager == null)
-			return;
+		if (RecipeManagers.fermenterManager == null) return;
 		liquidJuice = ItemInterface.getItem("liquidJuice");
 		liquidHoney = ItemInterface.getItem("liquidHoney");
 		liquidBiomass = ItemInterface.getItem("liquidBiomass");
 		if (ExtrabiomesBlock.sapling != null) {
-			addFermenterRecipeSapling(new ItemStack(ExtrabiomesBlock.sapling, 1, BlockCustomSapling.metaBrown));
-			addFermenterRecipeSapling(new ItemStack(ExtrabiomesBlock.sapling, 1, BlockCustomSapling.metaOrange));
-			addFermenterRecipeSapling(new ItemStack(ExtrabiomesBlock.sapling, 1, BlockCustomSapling.metaPurple));
-			addFermenterRecipeSapling(new ItemStack(ExtrabiomesBlock.sapling, 1, BlockCustomSapling.metaYellow));
-			addFermenterRecipeSapling(new ItemStack(ExtrabiomesBlock.sapling, 1, BlockCustomSapling.metaFir));
-			addFermenterRecipeSapling(new ItemStack(ExtrabiomesBlock.sapling, 1, BlockCustomSapling.metaRedWood));
-			addFermenterRecipeSapling(new ItemStack(ExtrabiomesBlock.sapling, 1, BlockCustomSapling.metaAcacia));
-		}		
+			addFermenterRecipeSapling(new ItemStack(
+					ExtrabiomesBlock.sapling, 1,
+					BlockCustomSapling.metaBrown));
+			addFermenterRecipeSapling(new ItemStack(
+					ExtrabiomesBlock.sapling, 1,
+					BlockCustomSapling.metaOrange));
+			addFermenterRecipeSapling(new ItemStack(
+					ExtrabiomesBlock.sapling, 1,
+					BlockCustomSapling.metaPurple));
+			addFermenterRecipeSapling(new ItemStack(
+					ExtrabiomesBlock.sapling, 1,
+					BlockCustomSapling.metaYellow));
+			addFermenterRecipeSapling(new ItemStack(
+					ExtrabiomesBlock.sapling, 1,
+					BlockCustomSapling.metaFir));
+			addFermenterRecipeSapling(new ItemStack(
+					ExtrabiomesBlock.sapling, 1,
+					BlockCustomSapling.metaRedWood));
+			addFermenterRecipeSapling(new ItemStack(
+					ExtrabiomesBlock.sapling, 1,
+					BlockCustomSapling.metaAcacia));
+		}
 	}
 
 	private static void addSaplings() {
-		if (ExtrabiomesBlock.sapling == null)
-			return;
+		if (ExtrabiomesBlock.sapling == null) return;
 
 		CropProviders.arborealCrops.add(new CropProviderSapling());
 		TerrainGenManager.treesCanGrowOnIDs.add(Integer
