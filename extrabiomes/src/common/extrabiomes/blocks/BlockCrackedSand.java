1e7e570113023d2e11f92a5493b5f071642c8e38
Scott Killen <scott@scottkillen.com>
2012-07-30 21:40:10 -0400

    Changed growth of cracked sand to a more logical progression. Results are that one block of cracked sand is not really effective against a healthy grassy plain or farmland, except by the most remote chance. Cracked sand en masse remains formidable. Growth sequence follow the prescribed pattern, except that hydrated farmland is slowly dried out and can recover when near water, however single water blocks surrounded on at least three sides by cracked sand are at risk.
    Closes issue #46.

--- extrabiomes/src/common/extrabiomes/blocks/BlockCrackedSand.java
+++ extrabiomes/src/common/extrabiomes/blocks/BlockCrackedSand.java
@@ -48,24 +48,48 @@ public class BlockCrackedSand extends Block implements ITextureProvider
 	private void changeNeighbor(World world, int x, int y, int z) {
 		if (world.getBlockLightValue(x, y + 1, z) < 9) return;
 
+		decayBlock(world, x, y, z);
+	}
+
+	private void decayBlock(World world, int x, int y, int z) {
 		final int id = world.getBlockId(x, y, z);
-		if (id == sand.blockID)
-			world.setBlockWithNotify(x, y, z, blockID);
+
+		if (id == tilledField.blockID) {
+			final int metadata = world.getBlockMetadata(x, y, z);
+			if (metadata != 0)
+				world.setBlockMetadataWithNotify(x, y, z, metadata - 1);
+			else
+				world.setBlockWithNotify(x, y, z, dirt.blockID);
+		} else if (id == grass.blockID)
+			world.setBlockWithNotify(x, y, z, dirt.blockID);
+		else if (id == dirt.blockID)
+			world.setBlockWithNotify(x, y, z, sandStone.blockID);
 		else if (id == sandStone.blockID) {
 			final int metadata = world.getBlockMetadata(x, y, z);
 			if (metadata != 0)
 				world.setBlockMetadataWithNotify(x, y, z, 0);
 			else
-				world.setBlockWithNotify(x, y, z, sand.blockID);
-		} else if (id == dirt.blockID)
-			world.setBlockWithNotify(x, y, z, sand.blockID);
-		else if (id == grass.blockID || id == tilledField.blockID)
-			world.setBlockWithNotify(x, y, z, dirt.blockID);
+				world.setBlockWithNotify(x, y, z, blockID);
+		} else if (id == waterStill.blockID)
+			if (isThreeSidesCrackedSand(world, x, y, z))
+				world.setBlockAndMetadataWithNotify(x, y, z,
+						waterMoving.blockID, 7);
 	}
 
 	@Override
 	public String getTextureFile() {
 		return "/extrabiomes/extrabiomes.png";
+	}
+
+	private boolean isThreeSidesCrackedSand(World world, int x, int y,
+			int z)
+	{
+		int count = 0;
+		for (int xTest = x - 1; xTest < x + 2; xTest += 2)
+			for (int zTest = z - 1; zTest < z + 2; zTest += 2)
+				if (world.getBlockId(xTest, y, zTest) == blockID)
+					count++;
+		return count >= 3;
 	}
 
 	@Override
