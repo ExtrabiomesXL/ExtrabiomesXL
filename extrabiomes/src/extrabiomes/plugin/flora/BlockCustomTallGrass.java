2e8f229310635a6f73bee77a5bfb73bb502dfcfd
Scott Killen <scott@scottkillen.com>
2012-09-16 23:08:25 -0400

    Fixed strange stacked grass generation.

--- extrabiomes/src/extrabiomes/plugin/flora/BlockCustomTallGrass.java
+++ extrabiomes/src/extrabiomes/plugin/flora/BlockCustomTallGrass.java
@@ -44,7 +44,10 @@ public class BlockCustomTallGrass extends BlockFlower implements
 	}
 
 	@Override
-	protected boolean canThisPlantGrowOnThisBlockID(int id) {
+	public boolean canBlockStay(World world, int x, int y,
+			int z)
+	{
+		int id = world.getBlockId(x, y - 1, z);
 		return (byte) id == BiomeManager.mountainridge.get().topBlock
 				|| (byte) id == BiomeManager.wasteland.get().topBlock
 				|| super.canThisPlantGrowOnThisBlockID(id);
