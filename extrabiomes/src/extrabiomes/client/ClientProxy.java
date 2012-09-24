d38a687f887ab122978e9e9bfb750fc24f42f618
ScottKillen <scott@scottkillen.com>
2012-09-23 23:43:33 -0400

    Removed unfinished redrock slabs and steps

--- extrabiomes/src/extrabiomes/client/ClientProxy.java
+++ extrabiomes/src/extrabiomes/client/ClientProxy.java
@@ -8,31 +8,14 @@
 package extrabiomes.client;
 
 import net.minecraftforge.client.MinecraftForgeClient;
-import cpw.mods.fml.client.registry.ISimpleBlockRenderingHandler;
 import cpw.mods.fml.client.registry.RenderingRegistry;
 import extrabiomes.CommonProxy;
-import extrabiomes.Extrabiomes;
-import extrabiomes.utility.RenderStairsHalfSlabs;
 
 public class ClientProxy extends CommonProxy {
-
-	private static boolean	rendersInitialized	= false;
 
 	@Override
 	public int getNextAvailableRenderId() {
 		return RenderingRegistry.getNextAvailableRenderId();
-	}
-
-	void initRenders() {
-		if (rendersInitialized) return;
-		rendersInitialized = true;
-
-		final ISimpleBlockRenderingHandler handler = new RenderStairsHalfSlabs();
-		Extrabiomes.setSlabRenderId(Extrabiomes.proxy.getNextAvailableRenderId());
-		Extrabiomes.setStairsRenderId(Extrabiomes.proxy.getNextAvailableRenderId());
-		
-		RenderingRegistry.registerBlockHandler(Extrabiomes.getSlabRenderId(), handler);
-		RenderingRegistry.registerBlockHandler(Extrabiomes.getStairsRenderId(), handler);
 	}
 
 	@Override
--- extrabiomes/src/extrabiomes/plugin/redrock/RedRock.java
+++ extrabiomes/src/extrabiomes/plugin/redrock/RedRock.java
@@ -15,7 +15,6 @@ import java.io.File;
 import java.util.logging.Level;
 
 import net.minecraft.src.Block;
-import net.minecraft.src.BlockHalfSlab;
 import net.minecraft.src.FurnaceRecipes;
 import net.minecraft.src.IRecipe;
 import net.minecraft.src.Item;
@@ -45,39 +44,17 @@ import extrabiomes.utility.EnhancedConfiguration;
 public class RedRock {
 
 	@SidedProxy(clientSide = "extrabiomes.client.ClientProxy", serverSide = "extrabiomes.CommonProxy")
-	public static CommonProxy							proxy;
+	public static CommonProxy		proxy;
 	@Instance("EBXLRedRock")
-	public static RedRock								instance;
+	public static RedRock			instance;
 
-	private static int									redRockId;
-	private static int									redRockHalfId;
-	private static int									redRockDoubleId;
-	private static int									redCobbleStepsId;
-	private static int									redRockBrickStepsId;
+	private static int				redRockId;
 
-	private static Optional<Block>						redRock;
-	private static Optional<Block>						redCobbleSteps;
-	private static Optional<Block>						redRockBrickSteps;
-
-	private static Optional<? extends BlockHalfSlab>	halfSlab;
-
-	private static Optional<? extends BlockHalfSlab>	doubleSlab;
-	private static final String							REDROCK_COMMENT	= "RedRock is used in terrain generation. Its id must be less than 256.";
-
-	public static BlockHalfSlab getDoubleSlab() {
-		return doubleSlab.get();
-	}
-
-	public static BlockHalfSlab getHalfSlab() {
-		return halfSlab.get();
-	}
+	private static Optional<Block>	redRock;
+	private static final String		REDROCK_COMMENT	= "RedRock is used in terrain generation. Its id must be less than 256.";
 
 	public static Optional<Block> getRedRockBlock() {
 		return redRock;
-	}
-
-	public static boolean halfSlabsEnabled() {
-		return halfSlab.isPresent();
 	}
 
 	@Init
@@ -94,73 +71,15 @@ public class RedRock {
 					.get().blockID));
 		}
 
-		if (0 < redRockHalfId) {
-			halfSlab = Optional.of(new BlockSlab(redRockHalfId, false));
-			halfSlab.get().setBlockName("redrockSlab");
-			proxy.setBlockHarvestLevel(halfSlab.get(), "pickaxe", 0);
-		}
-
-		if (0 < redRockDoubleId) {
-			doubleSlab = Optional.of(new BlockSlab(redRockDoubleId,
-					true));
-			doubleSlab.get().setBlockName("redrockSlab");
-			proxy.setBlockHarvestLevel(doubleSlab.get(), "pickaxe", 0);
-		}
-
 		proxy.registerRenderInformation();
-
-		if (halfSlab.isPresent())
-			proxy.registerBlock(halfSlab,
-					extrabiomes.plugin.redrock.ItemSlab.class);
-
-		if (doubleSlab.isPresent())
-			proxy.registerBlock(doubleSlab,
-					extrabiomes.plugin.redrock.ItemSlab.class);
-
-		if (0 < redCobbleStepsId) {
-			redCobbleSteps = Optional.of(new BlockRedRockStairs(
-					redCobbleStepsId, redRock.get(), RED_COBBLE
-							.metadata())
-					.setBlockName("stairsRedCobble"));
-			proxy.setBlockHarvestLevel(redCobbleSteps.get(), "pickaxe",
-					0);
-			proxy.registerBlock(redCobbleSteps);
-		}
-
-		if (0 < redRockBrickStepsId) {
-			redRockBrickSteps = Optional.of(new BlockRedRockStairs(
-					redRockBrickStepsId, redRock.get(), RED_ROCK_BRICK
-							.metadata())
-					.setBlockName("stairsRedRockBrick"));
-			proxy.setBlockHarvestLevel(redRockBrickSteps.get(),
-					"pickaxe", 0);
-			proxy.registerBlock(redRockBrickSteps);
-		}
 
 		registerRecipes();
 
-		for (final BlockType blockType : BlockType.values()) {
+		for (final BlockType blockType : BlockType.values())
 			if (redRock.isPresent())
 				proxy.addName(
 						new ItemStack(redRock.get(), 1, blockType
 								.metadata()), blockType.itemName());
-
-			if (halfSlab.isPresent())
-				proxy.addName(new ItemStack(halfSlab.get(), 1,
-						blockType.metadata()), blockType.itemName()
-						+ " Slab");
-
-			if (doubleSlab.isPresent())
-				proxy.addName(new ItemStack(doubleSlab.get(), 1,
-						blockType.metadata()), blockType.itemName()
-						+ " Double Slab");
-		}
-		if (redCobbleSteps.isPresent())
-			proxy.addName(redCobbleSteps.get(),
-					"Red Cobblestone Stairs");
-		if (redRockBrickSteps.isPresent())
-			proxy.addName(redRockBrickSteps.get(),
-					"Red Rock Brick Stairs");
 	}
 
 	public static boolean isEnabled() {
@@ -176,7 +95,7 @@ public class RedRock {
 		try {
 			cfg.load();
 
-			Property property = cfg
+			final Property property = cfg
 					.getOrCreateRestrictedBlockIdProperty("redrock.id",
 							158);
 			redRockId = property.getInt(0);
@@ -185,43 +104,6 @@ public class RedRock {
 			if (0 == redRockId)
 				ExtrabiomesLog
 						.info("redrock.id = 0, so red rock has been disabled.");
-
-			property = cfg.getOrCreateBlockIdProperty(
-					"redrock.halfslab.id", 160);
-			redRockHalfId = property.getInt(0);
-
-			if (0 == redRockHalfId)
-				ExtrabiomesLog
-						.info("redrock.halfslab.id = 0, so red rock slabs have been disabled.");
-
-			property = cfg.getOrCreateBlockIdProperty(
-					"redrock.doubleslab.id", 161);
-			redRockDoubleId = property.getInt(0);
-
-			if (0 == redRockDoubleId)
-				ExtrabiomesLog
-						.info("redrock.doubleslab.id = 0, so red rock slabs have been disabled.");
-
-			if (0 == redRockHalfId || 0 == redRockDoubleId) {
-				redRockHalfId = 0;
-				redRockDoubleId = 0;
-			}
-
-			property = cfg.getOrCreateBlockIdProperty(
-					"redrockbrick.steps.id", 163);
-			redRockBrickStepsId = property.getInt(0);
-
-			if (0 == redRockBrickStepsId)
-				ExtrabiomesLog
-						.info("redrockbrick.steps.id = 0, so red rock brick steps have been disabled.");
-
-			property = cfg.getOrCreateBlockIdProperty(
-					"redcobble.steps.id", 164);
-			redCobbleStepsId = property.getInt(0);
-
-			if (0 == redCobbleStepsId)
-				ExtrabiomesLog
-						.info("redcobble.steps.id = 0, so red cobble steps have been disabled.");
 
 		} catch (final Exception e) {
 			ExtrabiomesLog
@@ -255,37 +137,6 @@ public class RedRock {
 					4, RED_ROCK_BRICK.metadata()), new String[] { "rr",
 					"rr" }, 'r', "rockRed");
 			proxy.addRecipe(recipe);
-
-			if (halfSlab.isPresent()) {
-				recipe = new ShapedOreRecipe(new ItemStack(
-						halfSlab.get(), 6, RED_ROCK.metadata()),
-						new String[] { "rrr" }, 'r', "rockRed");
-				proxy.addRecipe(recipe);
-
-				recipe = new ShapedOreRecipe(new ItemStack(
-						halfSlab.get(), 6, RED_COBBLE.metadata()),
-						new String[] { "rrr" }, 'r', "cobbleRed");
-				proxy.addRecipe(recipe);
-
-				recipe = new ShapedOreRecipe(new ItemStack(
-						halfSlab.get(), 6, RED_ROCK_BRICK.metadata()),
-						new String[] { "rrr" }, 'r', "brickRedRock");
-				proxy.addRecipe(recipe);
-			}
-
-			if (redCobbleSteps.isPresent()) {
-				recipe = new ShapedOreRecipe(new ItemStack(
-						redCobbleSteps.get(), 4), new String[] { "r  ",
-						"rr ", "rrr" }, 'r', "cobbleRed");
-				proxy.addRecipe(recipe);
-			}
-
-			if (redRockBrickSteps.isPresent()) {
-				recipe = new ShapedOreRecipe(new ItemStack(
-						redRockBrickSteps.get(), 4), new String[] {
-						"r  ", "rr ", "rrr" }, 'r', "brickRedRock");
-				proxy.addRecipe(recipe);
-			}
 
 			FurnaceRecipes.smelting().addSmelting(
 					redRock.get().blockID, RED_COBBLE.metadata(),
