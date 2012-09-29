701c73a9144601545c61654b968e120b87939f08
ScottKillen <scott@scottkillen.com>
2012-09-28 21:50:00 -0400

    Added module control properties

--- extrabiomes/src/extrabiomes/ExtrabiomesConfig.java
+++ extrabiomes/src/extrabiomes/ExtrabiomesConfig.java
@@ -1,7 +1,7 @@
 /**
- * This mod is distributed under the terms of the Minecraft Mod Public
- * License 1.0, or MMPL. Please check the contents of the license
- * located in /MMPL-1.0.txt
+ * This work is licensed under the Creative Commons
+ * Attribution-ShareAlike 3.0 Unported License. To view a copy of this
+ * license, visit http://creativecommons.org/licenses/by-sa/3.0/.
  */
 
 package extrabiomes;
@@ -14,12 +14,16 @@ import extrabiomes.utility.EnhancedConfiguration;
 
 public class ExtrabiomesConfig extends EnhancedConfiguration {
 
-	public static final String			CATEGORY_BIOME	= "biome";
+	public static final String			CATEGORY_BIOME			= "biome";
+	public static final String			CATEGORY_MODULE_CONTROL	= "module_control";
 
-	public TreeMap<String, Property>	biomeProperties	= new TreeMap<String, Property>();
+	public TreeMap<String, Property>	biomeProperties			= new TreeMap<String, Property>();
+	public TreeMap<String, Property>	moduleControlProperties	= new TreeMap<String, Property>();
 
 	public ExtrabiomesConfig(File file) {
 		super(file);
 		categories.put(CATEGORY_BIOME, biomeProperties);
+		categories
+				.put(CATEGORY_MODULE_CONTROL, moduleControlProperties);
 	}
 }
