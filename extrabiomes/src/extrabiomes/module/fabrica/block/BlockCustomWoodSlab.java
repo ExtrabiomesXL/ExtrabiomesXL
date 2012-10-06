5c191a6f2faf889e4551b1ed2f375efe80c2ad97
ScottKillen <scott@scottkillen.com>
2012-10-06 00:32:50 -0400

    Add license text

--- extrabiomes/src/extrabiomes/module/fabrica/block/BlockCustomWoodSlab.java
+++ extrabiomes/src/extrabiomes/module/fabrica/block/BlockCustomWoodSlab.java
@@ -1,3 +1,9 @@
+/**
+ * This work is licensed under the Creative Commons
+ * Attribution-ShareAlike 3.0 Unported License. To view a copy of this
+ * license, visit http://creativecommons.org/licenses/by-sa/3.0/.
+ */
+
 
 package extrabiomes.module.fabrica.block;
 
