21e34cfed6ada5bec9c05c72b3be09a72064ed64
Scott Killen <scott@scottkillen.com>
2012-11-24 15:13:56 -0500

    Reformatted source file

--- extrabiomes/src/extrabiomes/core/helper/LogHelper.java
+++ extrabiomes/src/extrabiomes/core/helper/LogHelper.java
@@ -11,63 +11,53 @@ package extrabiomes.core.helper;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import cpw.mods.fml.common.FMLLog;
-
 public enum LogHelper {
-	INSTANCE;
- 
-	private static boolean configured;
-    private static Logger myLog;
+    INSTANCE;
 
-	public static void configureLogging() {
-		if (configured) return;
-		configured = true;
-		
-		myLog = Logger.getLogger("ExtrabiomesXL");
-		myLog.setParent(Logger.getLogger("ForgeModLoader"));
-	}
-	
-    public static void log(Level level, String format, Object... data)
-    {
-    	myLog.log(level, String.format(format, data));
+    private static boolean configured;
+    private static Logger  myLog;
+
+    public static void configureLogging() {
+        if (configured) return;
+        configured = true;
+
+        myLog = Logger.getLogger("ExtrabiomesXL");
+        myLog.setParent(Logger.getLogger("ForgeModLoader"));
     }
 
-    public static void log(Level level, Throwable ex, String format, Object... data)
-    {
-    	myLog.log(level, String.format(format, data), ex);
-    }
-
-    public static void severe(String format, Object... data)
-    {
-        log(Level.SEVERE, format, data);
-    }
-
-    public static void warning(String format, Object... data)
-    {
-        log(Level.WARNING, format, data);
-    }
-
-    public static void info(String format, Object... data)
-    {
-        log(Level.INFO, format, data);
-    }
-
-    public static void fine(String format, Object... data)
-    {
+    public static void fine(String format, Object... data) {
         log(Level.FINE, format, data);
     }
 
-    public static void finer(String format, Object... data)
-    {
+    public static void finer(String format, Object... data) {
         log(Level.FINER, format, data);
     }
 
-    public static void finest(String format, Object... data)
-    {
+    public static void finest(String format, Object... data) {
         log(Level.FINEST, format, data);
     }
-    public static Logger getLogger()
-    {
+
+    public static Logger getLogger() {
         return myLog;
+    }
+
+    public static void info(String format, Object... data) {
+        log(Level.INFO, format, data);
+    }
+
+    public static void log(Level level, String format, Object... data) {
+        myLog.log(level, String.format(format, data));
+    }
+
+    public static void log(Level level, Throwable ex, String format, Object... data) {
+        myLog.log(level, String.format(format, data), ex);
+    }
+
+    public static void severe(String format, Object... data) {
+        log(Level.SEVERE, format, data);
+    }
+
+    public static void warning(String format, Object... data) {
+        log(Level.WARNING, format, data);
     }
 }
