706a5d32b951cd4f7015370c6f3357e44c79e4d5
Scott Killen <scott@scottkillen.com>
2012-11-24 17:08:51 -0500

    Reformatted source code

--- extrabiomes/src/extrabiomes/configuration/EnhancedConfiguration.java
+++ extrabiomes/src/extrabiomes/configuration/EnhancedConfiguration.java
@@ -20,69 +20,60 @@ import com.google.common.base.Optional;
 
 public abstract class EnhancedConfiguration extends Configuration {
 
-	private static List<Integer>	assignedIdsList	= new ArrayList<Integer>();
+    private static List<Integer> assignedIdsList = new ArrayList<Integer>();
 
-	public static void releaseStaticResources() {
-		assignedIdsList = null;
-	}
+    public static void releaseStaticResources() {
+        assignedIdsList = null;
+    }
 
-	public EnhancedConfiguration(File file) {
-		super(file);
-	}
+    public EnhancedConfiguration(File file) {
+        super(file);
+    }
 
-	@Override
-	public Property getBlock(String key, int defaultId) {
-		return getBlock(key, defaultId, Block.blocksList.length);
-	}
+    @Override
+    public Property getBlock(String key, int defaultId) {
+        return getBlock(key, defaultId, Block.blocksList.length);
+    }
 
-	private Property getBlock(String key, int defaultId,
-			int exclusiveUpperBound)
-	{
-		Optional<? extends Map<String, Property>> properties = Optional
-				.fromNullable(categories.get(CATEGORY_BLOCK));
-		if (properties.isPresent() && properties.get().containsKey(key))
-		{
-			final Property property = get(CATEGORY_BLOCK, key,
-					defaultId);
-			assignedIdsList.add(Integer.parseInt(property.value));
-			return property;
-		} else {
-			if (!properties.isPresent()) {
-				properties = Optional
-						.of(new TreeMap<String, Property>());
-				categories.put(CATEGORY_BLOCK, properties.get());
-			}
-			final Property property = new Property();
-			properties.get().put(key, property);
-			property.setName(key);
+    private Property getBlock(String key, int defaultId, int exclusiveUpperBound) {
+        Optional<? extends Map<String, Property>> properties = Optional.fromNullable(categories
+                .get(CATEGORY_BLOCK));
+        if (properties.isPresent() && properties.get().containsKey(key)) {
+            final Property property = get(CATEGORY_BLOCK, key, defaultId);
+            assignedIdsList.add(Integer.parseInt(property.value));
+            return property;
+        } else {
+            if (!properties.isPresent()) {
+                properties = Optional.of(new TreeMap<String, Property>());
+                categories.put(CATEGORY_BLOCK, properties.get());
+            }
+            final Property property = new Property();
+            properties.get().put(key, property);
+            property.setName(key);
 
-			if (Block.blocksList[defaultId] == null
-					&& !assignedIdsList.contains(Integer
-							.valueOf(defaultId)))
-			{
-				property.value = Integer.toString(defaultId);
-				assignedIdsList.add(Integer.parseInt(property.value));
-				return property;
-			} else {
-				for (int j = exclusiveUpperBound - 1; j >= 0; --j)
-					if (Block.blocksList[j] == null
-							&& !assignedIdsList.contains(Integer
-									.valueOf(j)))
-					{
-						property.value = Integer.toString(j);
-						assignedIdsList.add(Integer
-								.parseInt(property.value));
-						return property;
-					}
+            if (Block.blocksList[defaultId] == null
+                    && !assignedIdsList.contains(Integer.valueOf(defaultId)))
+            {
+                property.value = Integer.toString(defaultId);
+                assignedIdsList.add(Integer.parseInt(property.value));
+                return property;
+            } else {
+                for (int j = exclusiveUpperBound - 1; j >= 0; --j)
+                    if (Block.blocksList[j] == null
+                            && !assignedIdsList.contains(Integer.valueOf(j)))
+                    {
+                        property.value = Integer.toString(j);
+                        assignedIdsList.add(Integer.parseInt(property.value));
+                        return property;
+                    }
 
-				throw new RuntimeException(
-						"No more block ids available for " + key);
-			}
-		}
-	}
+                throw new RuntimeException("No more block ids available for " + key);
+            }
+        }
+    }
 
-	public Property getRestrictedBlock(String key, int defaultId) {
-		return getBlock(key, defaultId, 256);
-	}
+    public Property getRestrictedBlock(String key, int defaultId) {
+        return getBlock(key, defaultId, 256);
+    }
 
 }
--- extrabiomes/src/extrabiomes/configuration/ExtrabiomesConfig.java
+++ extrabiomes/src/extrabiomes/configuration/ExtrabiomesConfig.java
@@ -13,44 +13,40 @@ import net.minecraftforge.common.Property;
 
 public class ExtrabiomesConfig extends EnhancedConfiguration {
 
-	public static final String	CATEGORY_BIOME			= "biome";
-	public static final String	CATEGORY_MODULE_CONTROL	= "module_control";
+    public static final String CATEGORY_BIOME          = "biome";
+    public static final String CATEGORY_MODULE_CONTROL = "module_control";
 
-	private static boolean[]	configBiomes			= new boolean[BiomeGenBase.biomeList.length];
+    private static boolean[]   configBiomes            = new boolean[BiomeGenBase.biomeList.length];
 
-	public ExtrabiomesConfig(File file) {
-		super(file);
-	}
+    public ExtrabiomesConfig(File file) {
+        super(file);
+    }
 
-	public Property getBiome(String key, int defaultID) {
-		return getBiome(CATEGORY_BIOME, key, defaultID);
-	}
+    public Property getBiome(String key, int defaultID) {
+        return getBiome(CATEGORY_BIOME, key, defaultID);
+    }
 
-	public Property getBiome(String category, String key, int defaultID)
-	{
-		final Property prop = get(category, key, -1);
+    public Property getBiome(String category, String key, int defaultID) {
+        final Property prop = get(category, key, -1);
 
-		if (prop.getInt() != -1) {
-			configBiomes[prop.getInt()] = true;
-			return prop;
-		}
+        if (prop.getInt() != -1) {
+            configBiomes[prop.getInt()] = true;
+            return prop;
+        }
 
-		if (BiomeGenBase.biomeList[defaultID] == null
-				&& !configBiomes[defaultID])
-		{
-			prop.value = Integer.toString(defaultID);
-			configBiomes[defaultID] = true;
-			return prop;
-		}
+        if (BiomeGenBase.biomeList[defaultID] == null && !configBiomes[defaultID]) {
+            prop.value = Integer.toString(defaultID);
+            configBiomes[defaultID] = true;
+            return prop;
+        }
 
-		for (int j = configBiomes.length - 1; j > 0; j--)
-			if (BiomeGenBase.biomeList[j] == null && !configBiomes[j]) {
-				prop.value = Integer.toString(j);
-				configBiomes[j] = true;
-				return prop;
-			}
+        for (int j = configBiomes.length - 1; j > 0; j--)
+            if (BiomeGenBase.biomeList[j] == null && !configBiomes[j]) {
+                prop.value = Integer.toString(j);
+                configBiomes[j] = true;
+                return prop;
+            }
 
-		throw new RuntimeException("No more biome ids available for "
-				+ key);
-	}
+        throw new RuntimeException("No more biome ids available for " + key);
+    }
 }
