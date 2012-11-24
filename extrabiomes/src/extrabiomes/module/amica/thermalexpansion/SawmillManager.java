e02b210582c396ac4e1bc6ba1aba750f6b6a7866
Scott Killen <scott@scottkillen.com>
2012-11-23 23:04:18 -0500

    Reformatted file

--- extrabiomes/src/extrabiomes/module/amica/thermalexpansion/SawmillManager.java
+++ extrabiomes/src/extrabiomes/module/amica/thermalexpansion/SawmillManager.java
@@ -14,46 +14,40 @@ import com.google.common.base.Optional;
 
 class SawmillManager {
 
-	private final Object			sawmill;
+    private final Object           sawmill;
 
-	/**
-	 * boolean addRecipe(int energy, ItemStack input, ItemStack primary,
-	 * ItemStack secondary, int secondaryChance);
-	 */
-	private final Optional<Method>	addRecipe;
+    /**
+     * boolean addRecipe(int energy, ItemStack input, ItemStack primary,
+     * ItemStack secondary, int secondaryChance);
+     */
+    private final Optional<Method> addRecipe;
 
-	SawmillManager(Object sawmill) throws Exception {
-		this.sawmill = sawmill;
+    SawmillManager(Object sawmill) throws Exception {
+        this.sawmill = sawmill;
 
-		addRecipe = Optional.fromNullable(sawmill.getClass().getMethod(
-				"addRecipe", Integer.TYPE, ItemStack.class,
-				ItemStack.class, ItemStack.class, Integer.TYPE));
-	}
+        addRecipe = Optional.fromNullable(sawmill.getClass().getMethod("addRecipe", Integer.TYPE,
+                ItemStack.class, ItemStack.class, ItemStack.class, Integer.TYPE));
+    }
 
-	boolean addRecipe(int energy, ItemStack input,
-			ItemStack primaryOutput)
-	{
-		return addRecipe(energy, input, primaryOutput, null, 0);
-	}
+    boolean addRecipe(int energy, ItemStack input, ItemStack primaryOutput) {
+        return addRecipe(energy, input, primaryOutput, null, 0);
+    }
 
-	boolean addRecipe(int energy, ItemStack input,
-			ItemStack primaryOutput, ItemStack secondaryOutput)
-	{
-		return addRecipe(energy, input, primaryOutput, secondaryOutput,
-				100);
-	}
+    boolean addRecipe(int energy, ItemStack input, ItemStack primaryOutput,
+            ItemStack secondaryOutput)
+    {
+        return addRecipe(energy, input, primaryOutput, secondaryOutput, 100);
+    }
 
-	boolean addRecipe(int energy, ItemStack input, ItemStack primary,
-			ItemStack secondary, int secondaryChance)
-	{
-		try {
-			addRecipe.get().invoke(sawmill, energy, input, primary,
-					secondary, secondaryChance);
-			return true;
-		} catch (final IllegalStateException e) {} catch (final Exception e)
-		{
-			e.printStackTrace();
-		}
-		return false;
-	}
+    boolean addRecipe(int energy, ItemStack input, ItemStack primary, ItemStack secondary,
+            int secondaryChance)
+    {
+        try {
+            addRecipe.get().invoke(sawmill, energy, input, primary, secondary, secondaryChance);
+            return true;
+        } catch (final IllegalStateException e) {} catch (final Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
 }
--- extrabiomes/src/extrabiomes/module/amica/thermalexpansion/ThermalExpansionAPI.java
+++ extrabiomes/src/extrabiomes/module/amica/thermalexpansion/ThermalExpansionAPI.java
@@ -7,33 +7,29 @@
 package extrabiomes.module.amica.thermalexpansion;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-
-import net.minecraft.src.BiomeGenBase;
-import net.minecraft.src.ItemStack;
 
 import com.google.common.base.Optional;
 
 class ThermalExpansionAPI {
-	
-	private Optional<SawmillManager> sawmill = Optional.absent();
 
-	public Optional<SawmillManager> getSawmill() {
-		return sawmill;
-	}
+    private Optional<SawmillManager> sawmill = Optional.absent();
 
-	ThermalExpansionAPI() {
+    ThermalExpansionAPI() {
 
-		Optional<Object> sawmillManager = Optional.absent();
-		
-		try {
-			Class cls = Class.forName("thermalexpansion.api.crafting.CraftingManagers");
-			Field fld = cls.getField("sawmillManager");
-			sawmillManager = Optional.of(fld.get(null));
-			sawmill = Optional.of(new SawmillManager(sawmillManager));
-		} catch (final Exception e) {
-			e.printStackTrace();
-			sawmill = Optional.absent();
-		}
-	}
+        Optional<Object> sawmillManager = Optional.absent();
+
+        try {
+            final Class cls = Class.forName("thermalexpansion.api.crafting.CraftingManagers");
+            final Field fld = cls.getField("sawmillManager");
+            sawmillManager = Optional.of(fld.get(null));
+            sawmill = Optional.of(new SawmillManager(sawmillManager));
+        } catch (final Exception e) {
+            e.printStackTrace();
+            sawmill = Optional.absent();
+        }
+    }
+
+    public Optional<SawmillManager> getSawmill() {
+        return sawmill;
+    }
 }
