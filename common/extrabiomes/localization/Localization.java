78c33b7a3b835e8942f37c48bf71e7b57f7cfec7
mcc <mccjohnny@gmail.com>
2013-08-25 18:42:37 +0800

    add Traditional Chinese translation update Simplefied Chinese locale var name

--- common/extrabiomes/localization/Localization.java
+++ common/extrabiomes/localization/Localization.java
@@ -8,7 +8,7 @@ package extrabiomes.localization;
 
 public enum Localization {
     US("en_US"), GERMAN("de_DE"), FRENCH("fr_FR"), PORTUGAL("pt_PT"), BRAZIL("pt_BR"), ITALIAN(
-            "it_IT"), RUSSIAN("ru_RU"), CHINESE("zh_CN"), SPANISH_AR("es_AR"), SPANISH_ES("es_ES"),
+            "it_IT"), RUSSIAN("ru_RU"), CHINESE_SC("zh_CN"),CHINESE_TC("zh_TW"), SPANISH_AR("es_AR"), SPANISH_ES("es_ES"),
             SPANISH_MX("es_MX"), SPANISH_UY("es_UY"), SPANISH_VE("es_VE")/*
                                        * CZECH("cs_CZ"), WELSH("cy_GB"),
                                        * FRENCH("fr_FR"),
