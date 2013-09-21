be24fe48eca87ddcec7f72441ed71a1f073e73c9
bspkrs <bspkrs@gmail.com>
2013-09-20 20:18:34 -0400

    bumped version to 3.14.2 for 1.6.4 update

--- common/extrabiomes/lib/Reference.java
+++ common/extrabiomes/lib/Reference.java
@@ -6,13 +6,13 @@
 
 package extrabiomes.lib;
 
-public abstract class Reference {
-
+public abstract class Reference
+{
     // Mod information
-    public static final String MOD_ID = "ExtrabiomesXL";
-    public static final String MOD_NAME = MOD_ID;
-    public static final String MOD_VERSION = "3.14.1";
+    public static final String MOD_ID       = "ExtrabiomesXL";
+    public static final String MOD_NAME     = MOD_ID;
+    public static final String MOD_VERSION  = "3.14.2";
     public static final String CLIENT_PROXY = "extrabiomes.proxy.ClientProxy";
     public static final String SERVER_PROXY = "extrabiomes.proxy.CommonProxy";
-
+    
 }
