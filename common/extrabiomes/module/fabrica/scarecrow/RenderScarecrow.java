383221979c25e9212dcf99f3ba985dd86ec5ff09
bspkrs <bspkrs@gmail.com>
2013-09-20 20:17:03 -0400

    updated for latest MCP mappings, 1.6.4. reformatted

--- common/extrabiomes/module/fabrica/scarecrow/RenderScarecrow.java
+++ common/extrabiomes/module/fabrica/scarecrow/RenderScarecrow.java
@@ -8,11 +8,10 @@ package extrabiomes.module.fabrica.scarecrow;
 
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.renderer.entity.RenderLiving;
-import net.minecraft.util.ResourceLocation;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.passive.EntityBat;
+import net.minecraft.util.ResourceLocation;
 
 import org.lwjgl.opengl.GL11;
 
@@ -21,44 +20,49 @@ import cpw.mods.fml.relauncher.SideOnly;
 import extrabiomes.Extrabiomes;
 
 @SideOnly(Side.CLIENT)
-public class RenderScarecrow extends RenderLiving {
-	private static final ResourceLocation model_resource = new ResourceLocation(Extrabiomes.TEXTURE_PATH + "textures/models/scarecrow.png");
-	
-	public RenderScarecrow(ModelBase modelbase, float f) {
-		super(modelbase, f);
-	}
-
-	@Override
-	public void doRender(Entity entity, double d, double d1, double d2,
-			float f, float f1)
-	{
-		renderScarecrow((EntityScarecrow) entity, d, d1, d2, f, f1);
-	}
-
-	@Override
-	public void doRenderLiving(EntityLiving entityliving, double d, double d1, double d2, float f, float f1) {
-		renderScarecrow((EntityScarecrow) entityliving, d, d1, d2, f, f1);
-	}
-
-	@Override
-	protected void preRenderCallback(EntityLivingBase entityliving, float f) {
-		preRenderScale((EntityScarecrow) entityliving, f);
-	}
-
-	protected void preRenderScale(EntityScarecrow entity, float f) {
-		GL11.glScalef(1.25F, 1.25F, 1.25F);
-	}
-
-	public void renderScarecrow(EntityScarecrow entityScarecrow,
-			double d, double d1, double d2, float f, float f1)
-	{
-		super.doRenderLiving(entityScarecrow, d, d1, d2, f, f1);
-	}
-
-	@Override
-	protected ResourceLocation func_110775_a(Entity entity) {
-		// TODO Auto-generated method stub
-		return model_resource;
-		//return new ResourceLocation(Extrabiomes.TEXTURE_PATH + "scarecrow");
-	}
+public class RenderScarecrow extends RenderLiving
+{
+    private static final ResourceLocation model_resource = new ResourceLocation(Extrabiomes.TEXTURE_PATH + "textures/models/scarecrow.png");
+    
+    public RenderScarecrow(ModelBase modelbase, float f)
+    {
+        super(modelbase, f);
+    }
+    
+    @Override
+    public void doRender(Entity entity, double d, double d1, double d2,
+            float f, float f1)
+    {
+        renderScarecrow((EntityScarecrow) entity, d, d1, d2, f, f1);
+    }
+    
+    @Override
+    public void doRenderLiving(EntityLiving entityliving, double d, double d1, double d2, float f, float f1)
+    {
+        renderScarecrow((EntityScarecrow) entityliving, d, d1, d2, f, f1);
+    }
+    
+    @Override
+    protected void preRenderCallback(EntityLivingBase entityliving, float f)
+    {
+        preRenderScale((EntityScarecrow) entityliving, f);
+    }
+    
+    protected void preRenderScale(EntityScarecrow entity, float f)
+    {
+        GL11.glScalef(1.25F, 1.25F, 1.25F);
+    }
+    
+    public void renderScarecrow(EntityScarecrow entityScarecrow,
+            double d, double d1, double d2, float f, float f1)
+    {
+        super.doRenderLiving(entityScarecrow, d, d1, d2, f, f1);
+    }
+    
+    @Override
+    protected ResourceLocation getEntityTexture(Entity entity)
+    {
+        return model_resource;
+        //return new ResourceLocation(Extrabiomes.TEXTURE_PATH + "scarecrow");
+    }
 }
