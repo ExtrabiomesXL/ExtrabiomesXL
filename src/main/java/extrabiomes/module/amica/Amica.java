c87ac8baf8b85fb118bd3221da13018fff8e2df9
Mr. Fibre <misterfibre@gmail.com>
2013-12-29 20:08:30 -0800

    readded Treecapitator IMC support

--- src/main/java/extrabiomes/module/amica/Amica.java
+++ src/main/java/extrabiomes/module/amica/Amica.java
@@ -18,10 +18,10 @@ import extrabiomes.module.amica.newdawn.NewDawnPlugin;
 
 public class Amica
 {
-
+    
     public static final String LOG_MESSAGE_PLUGIN_ERROR = "log.message.plugin.error";
     public static final String LOG_MESSAGE_PLUGIN_INIT  = "log.message.plugin.init";
-
+    
     @ForgeSubscribe(priority = EventPriority.LOWEST)
     public void init(ModuleInitEvent event) throws InstantiationException, IllegalAccessException
     {
@@ -31,5 +31,5 @@ public class Amica
         Api.registerPlugin(new ATGPlugin());
         Api.registerPlugin(new NewDawnPlugin());
     }
-
+    
 }
