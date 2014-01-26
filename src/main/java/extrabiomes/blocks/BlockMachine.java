ce9440126f0849deff1f88373a0383f7ba24b3bd
Ammon Lauritzen <allaryin@gmail.com>
2014-01-26 02:52:10 -0600

    More debug spam to help track down what's going wrong (#1)

--- src/main/java/extrabiomes/blocks/BlockMachine.java
+++ src/main/java/extrabiomes/blocks/BlockMachine.java
@@ -1,14 +1,19 @@
 package extrabiomes.blocks;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.network.packet.Packet51MapChunk;
 import net.minecraft.server.management.PlayerInstance;
+import net.minecraft.util.Icon;
+import net.minecraft.util.Vec3;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldServer;
 import net.minecraft.world.biome.BiomeGenBase;
@@ -17,9 +22,13 @@ import net.minecraft.world.chunk.storage.IChunkLoader;
 import net.minecraft.world.gen.ChunkProviderServer;
 import net.minecraftforge.common.DimensionManager;
 
-import com.google.common.base.Optional;
-import com.google.common.collect.Lists;
+import org.apache.commons.lang3.tuple.Pair;
 
+import com.google.common.base.Optional;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import extrabiomes.Extrabiomes;
 import extrabiomes.handlers.GenesisBiomeOverrideHandler;
 import extrabiomes.helpers.LogHelper;
 import extrabiomes.lib.BiomeSettings;
@@ -27,10 +36,24 @@ import extrabiomes.lib.GenesisChunkProvider;
 
 public class BlockMachine extends Block {
 
-	static boolean				doingGenesis	= false;
+	static boolean	doingGenesis	= false;
 
 	public BlockMachine(int id) {
 		super(id, Material.iron);
+		// setCreativeTab(CreativeTabs.blocks);
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void registerIcons(IconRegister iconRegister) {
+		System.out.println("Registering icon for " + getClass().getName());
+		blockIcon = iconRegister.registerIcon(Extrabiomes.TEXTURE_PATH
+				+ "genesis");
+	}
+
+	@Override
+	public Icon getIcon(int side, int metadata) {
+		return blockIcon;
 	}
 
 	@Override
@@ -45,186 +68,218 @@ public class BlockMachine extends Block {
 	}
 
 	@SuppressWarnings("unchecked")
+	@SideOnly(Side.SERVER)
 	public void doGenesis(World world, int x, int y, int z, EntityPlayer sender) {
 		// pick a new biome
-		BiomeSettings[] biomes = BiomeSettings.values();
+		LogHelper.info("Starting genesis at " + x + "," + z);
 
-		BiomeGenBase oldBiome = world.getBiomeGenForCoords(x, z);
+		final BiomeSettings[] biomes = BiomeSettings.values();
+		// final BiomeSettings[] biomes = { BiomeSettings.AUTUMNWOODS,
+		// BiomeSettings.DESERT, BiomeSettings.GREENSWAMP };
+
+		final BiomeGenBase oldBiome = world.getBiomeGenForCoords(x, z);
 		BiomeGenBase newBiome = null;
+		// newBiome = BiomeSettings.AUTUMNWOODS.getBiome().get();
+
 		do {
-			Optional<?extends BiomeGenBase> opt = biomes[world.rand.nextInt(biomes.length)].getBiome();
+			Optional<? extends BiomeGenBase> opt = biomes[world.rand
+					.nextInt(biomes.length)].getBiome();
 			if (opt.isPresent()) newBiome = opt.get();
 		} while (oldBiome == newBiome || newBiome == null);
-		
 
-	    GenesisBiomeOverrideHandler.enable(newBiome.biomeID);
-		
+		LogHelper.info("Old biome was " + oldBiome);
+		LogHelper.info("New biome will be " + newBiome);
+
+		GenesisBiomeOverrideHandler.enable(newBiome.biomeID);
+
 		int range = 3;
-	    Integer dimension = null;
-	    Integer chunkX = sender.getPlayerCoordinates().posX >> 4;
-	    Integer chunkZ = sender.getPlayerCoordinates().posZ >> 4;
-	    dimension = Integer.valueOf(sender.dimension);
-	    if ((chunkX == null) || (chunkZ == null))
-	    { 
-	      chunkX = Integer.valueOf((int)sender.posX >> 4);
-	      chunkZ = Integer.valueOf((int)sender.posZ >> 4);
-	    }
-	    WorldServer worldObj = DimensionManager.getWorld(dimension.intValue());
-	    if (worldObj == null) {
-	      //throw new CommandException("The target world is not loaded", new Object[0]);
-	    }
-	    ChunkProviderServer chunkprovider = (ChunkProviderServer)worldObj.getChunkProvider();
-	    GenesisChunkProvider provider = new GenesisChunkProvider(world, newBiome);
-	    
-	    List<EntityPlayerMP> players = new ArrayList();
-	    players.addAll(worldObj.playerEntities);
-	    for (int x1 = chunkX.intValue() - range; x1 <= chunkX.intValue() + range; x1++) {
-	      for (int z1 = chunkZ.intValue() - range; z1 <= chunkZ.intValue() + range; z1++)
-	      {
-	        for (EntityPlayerMP player : players) {
-	          if (worldObj.getPlayerManager().isPlayerWatchingChunk(player, x1, z1))
-	          {
-	            player.setLocationAndAngles(chunkX.intValue() - range - 2 << 4, player.posY, chunkZ.intValue() - range - 2 << 4, 0.0F, 0.0F);
-	            worldObj.updateEntityWithOptionalForce(player, false);
-	          }
-	        }
-	        chunkprovider.unloadChunksIfNotNearSpawn(x1, z1);
-	        
-	      }
-	    }
-	    
-	    Chunk chunk = world.getChunkFromBlockCoords(x, z);
-	    
-	    int lastloaded = 0;
-	    while (chunkprovider.getLoadedChunkCount() != lastloaded)
-	    {
-	      lastloaded = chunkprovider.getLoadedChunkCount();
-	      chunkprovider.unloadQueuedChunks();
-	      
-	      if (chunk != null) {
-				if( chunk.isChunkLoaded ) {
-					LogHelper.warning("Failed to unload chunk!");
-					//loadCount = 0;
-				} else {
-					LogHelper.info("Chunk unloaded.");
-				}
-				chunk = null; // better safe than sorry
-			}
-	    }
-	    System.out.println(chunkprovider.makeString());
-	    
-	    
-	    IChunkLoader chunkloader = chunkprovider.currentChunkLoader;
-	    chunkprovider.currentChunkLoader = null;
-	    for (int x1 = chunkX.intValue() - range; x1 <= chunkX.intValue() + range; x1++) {
-	      for (int z1 = chunkZ.intValue() - range; z1 <= chunkZ.intValue() + range; z1++) {
-	    	//GenesisBiomeOverrideHandler.myQueue.add(new GenesisBiomeOverrideHandler.coord(x1, z1));
-	    	  
-	    	provider.loadChunk(x1, z1);
-	    	provider.populate(provider, x1, z1);
-	        
-	      }
-	    }
-	    
-	    
-	    chunkprovider.currentChunkLoader = chunkloader;
-	    
-	    //notifyAdmins(sender, String.format("%s regenerated chunks (%d, %d)+-%d in Dimension %d", new Object[] { sender.getCommandSenderName(), chunkX, chunkZ, Integer.valueOf(range), Integer.valueOf(caller.dimension) }), new Object[0]);
-	    for (int x1 = chunkX.intValue() - range; x1 <= chunkX.intValue() + range; x1++) {
-	      for (int z1 = chunkZ.intValue() - range; z1 <= chunkZ.intValue() + range; z1++)
-	      {
-	        PlayerInstance chunkwatcher = worldObj.getPlayerManager().getOrCreateChunkWatcher(x1, z1, false);
-	        if (chunkwatcher != null) {
-	          chunkwatcher.sendToAllPlayersWatchingChunk(new Packet51MapChunk(worldObj.getChunkFromChunkCoords(x1, z1), true, -1));
-	        }
-	      }
-	    }
-	    
-	    GenesisBiomeOverrideHandler.disable();
-		
-		/*
-		LogHelper.info("GENESIS @ " + x + "," + y + "," + z + " by "
-				+ player.getDisplayName());
-		
-		world.setBlockToAir(x, y, z);
-		WorldServer worldServer = (WorldServer) world;
-
-		
-
-		LogHelper.info(oldBiome.biomeName + " >> " + newBiome.biomeName);
-		
-		// update chunk's biome values
-		Chunk chunk = world.getChunkFromBlockCoords(x, z);
-		byte[] chunkBiomes = chunk.getBiomeArray();
-		for (int k = 0; k < chunkBiomes.length; ++k) {
-			chunkBiomes[k] = (byte) ( newBiome.biomeID & 0xFF );
+		Integer dimension = null;
+		Integer chunkX = sender.getPlayerCoordinates().posX >> 4;
+		Integer chunkZ = sender.getPlayerCoordinates().posZ >> 4;
+		dimension = Integer.valueOf(sender.dimension);
+		if (( chunkX == null ) || ( chunkZ == null )) {
+			chunkX = Integer.valueOf((int) sender.posX >> 4);
+			chunkZ = Integer.valueOf((int) sender.posZ >> 4);
 		}
-		chunk.setBiomeArray(chunkBiomes);
-		chunk.needsSaving(true);
-
-		LogHelper.info("-1");
-
-		int chunkX = chunk.xPosition;
-		int chunkZ = chunk.zPosition;
-
-		// TODO: move all players in the blast radius out of the way
-		List<EntityPlayerMP> players = Lists.newArrayList();
-		players.addAll(worldServer.playerEntities);
-		for (EntityPlayerMP p : players) {
-			if( worldServer.getPlayerManager().isPlayerWatchingChunk(p, chunkX, chunkZ)) {
-				LogHelper.info(" - pushing " + p);
-				p.setLocationAndAngles((chunkX - 5) << 4, p.posY,
-						(chunkZ - 5) << 4, 0F, 0F);
-				worldServer.updateEntityWithOptionalForce(p, false);
-			}
+		WorldServer worldObj = DimensionManager.getWorld(dimension.intValue());
+		if (worldObj == null) {
+			LogHelper.warning("Target world " + dimension.intValue()
+					+ " is not loaded for genesis?!");
+			// throw new CommandException("The target world is not loaded", new
+			// Object[0]);
 		}
-
-		LogHelper.info("0");
-
-		// unload affected chunk
-		GenesisChunkProvider provider = new GenesisChunkProvider(world,
+		ChunkProviderServer providerServer = (ChunkProviderServer) worldObj
+				.getChunkProvider();
+		GenesisChunkProvider providerGenesis = new GenesisChunkProvider(world,
 				newBiome);
-		provider.unloadChunksIfNotNearSpawn(chunkX, chunkZ);
 
-		int loadCount = 0;
-		while (provider.getLoadedChunkCount() != loadCount) {
-			loadCount = provider.getLoadedChunkCount();
-			provider.unloadQueuedChunks();
-			if (chunk != null) {
-				if( chunk.isChunkLoaded ) {
-					LogHelper.warning("Failed to unload chunk!");
-					loadCount = 0;
-				} else {
-					LogHelper.info("Chunk unloaded.");
+		// clear players from the danger zone and force unload chunks in
+		// question
+		List<EntityPlayerMP> players = new ArrayList<EntityPlayerMP>();
+		Map<EntityPlayerMP, Vec3> playerOrigPositions = new HashMap<EntityPlayerMP, Vec3>();
+		players.addAll(worldObj.playerEntities);
+
+		final int safePad = 2;
+		final double safeX = ( ( chunkX.intValue() - range - safePad ) << 4 ) - 8.0;
+		final double safeZ = ( ( chunkZ.intValue() - range - safePad ) << 4 ) - 8.0;
+		LogHelper.info("Safe position = " + safeX + "," + safeZ);
+
+		Map<Pair<Integer, Integer>, Chunk> chunks = new HashMap<Pair<Integer, Integer>, Chunk>();
+		for (int x1 = chunkX.intValue() - range; x1 <= chunkX.intValue()
+				+ range; x1++) {
+			for (int z1 = chunkZ.intValue() - range; z1 <= chunkZ.intValue()
+					+ range; z1++) {
+				for (EntityPlayerMP player : players) {
+					if (worldObj.getPlayerManager().isPlayerWatchingChunk(
+							player, x1, z1)
+							&& !playerOrigPositions.containsKey(player)) {
+						final Vec3 origPosition = Vec3.createVectorHelper(player.posX, player.posY,player.posZ);
+						playerOrigPositions.put(player, origPosition);
+						LogHelper.info("Moving player " + player);
+						player.setLocationAndAngles(safeX, player.posY, safeZ,
+								0.0F, 0.0F);
+						worldObj.updateEntityWithOptionalForce(player, true);
+					}
 				}
-				chunk = null; // better safe than sorry
+				final Chunk chunk = world.getChunkFromBlockCoords(x, z);
+				chunks.put(Pair.of(x1, z1), chunk); // save map of chunks
+				providerServer.unloadChunksIfNotNearSpawn(x1, z1);
 			}
 		}
 
-		LogHelper.info("1");
+		// verify that chunks unloaded
+		int lastloaded = 0;
+		while (providerServer.getLoadedChunkCount() != lastloaded) {
+			lastloaded = providerServer.getLoadedChunkCount();
+			providerServer.unloadQueuedChunks();
 
-		// regenerate the chunk
-		IChunkLoader loader = provider.getCurrentChunkLoader();
-		provider.setCurrentChunkLoader(null);
-		provider.loadChunk(chunkX, chunkZ);
-		provider.populate(provider, chunkX, chunkZ);
-		provider.setCurrentChunkLoader(loader);
+			for (final Pair<Integer, Integer> coord : chunks.keySet()) {
+				final Chunk chunk = chunks.get(coord);
+				if (chunk != null) {
+					if (chunk.isChunkLoaded) {
+						LogHelper.warning("Failed to unload chunk @ " + coord);
+						/*
+						 * } else { LogHelper.info("Chunk unloaded @ " + coord);
+						 */
+					}
+				}
+			}
+		}
+		chunks.clear();
+		LogHelper.info(providerServer.makeString());
 
-		LogHelper.info("2");
+		IChunkLoader chunkloader = providerServer.currentChunkLoader;
+		providerServer.currentChunkLoader = null;
+		for (int x1 = chunkX.intValue() - range; x1 <= chunkX.intValue()
+				+ range; x1++) {
+			for (int z1 = chunkZ.intValue() - range; z1 <= chunkZ.intValue()
+					+ range; z1++) {
+				// GenesisBiomeOverrideHandler.myQueue.add(new
+				// GenesisBiomeOverrideHandler.coord(x1, z1));
 
-		PlayerInstance watcher = worldServer.getPlayerManager()
-				.getOrCreateChunkWatcher(chunkX, chunkZ, false);
-		if (watcher != null) {
-			chunk = world.getChunkFromChunkCoords(chunkZ, chunkZ);
-			Packet51MapChunk packet = new Packet51MapChunk(chunk, true, -1);
-			watcher.sendToAllPlayersWatchingChunk(packet);
-		} else {
-			LogHelper.warning("Unable to find chunk watcher!");
+				final Chunk chunk = providerGenesis.loadChunk(x1, z1);
+				providerGenesis.populate(providerGenesis, x1, z1);
+				/*
+				 * // re-attempt to force set biome on chunk byte[] chunkBiomes
+				 * = chunk.getBiomeArray(); for (int k = 0; k < biomes.length;
+				 * ++k) { chunkBiomes[k] = (byte) newBiome.biomeID; }
+				 * chunk.setBiomeArray(chunkBiomes);
+				 */
+				chunk.setChunkModified();
+			}
 		}
 
-		LogHelper.info("3");
-		
-		*/
+		providerServer.currentChunkLoader = chunkloader;
+
+		// notifyAdmins(sender,
+		// String.format("%s regenerated chunks (%d, %d)+-%d in Dimension %d",
+		// new Object[] { sender.getCommandSenderName(), chunkX, chunkZ,
+		// Integer.valueOf(range), Integer.valueOf(caller.dimension) }), new
+		// Object[0]);
+		for (int x1 = chunkX.intValue() - range; x1 <= chunkX.intValue()
+				+ range; x1++) {
+			for (int z1 = chunkZ.intValue() - range; z1 <= chunkZ.intValue()
+					+ range; z1++) {
+				PlayerInstance chunkwatcher = worldObj.getPlayerManager()
+						.getOrCreateChunkWatcher(x1, z1, false);
+				if (chunkwatcher != null) {
+					chunkwatcher
+							.sendToAllPlayersWatchingChunk(new Packet51MapChunk(
+									worldObj.getChunkFromChunkCoords(x1, z1),
+									true, -1));
+				}
+			}
+		}
+
+		GenesisBiomeOverrideHandler.disable();
+
+		// TODO: restore players to their original saved positions
+		for (EntityPlayerMP player : players) {
+			LogHelper.info(player.toString());
+		}
+
+		/*
+		 * LogHelper.info("GENESIS @ " + x + "," + y + "," + z + " by " +
+		 * player.getDisplayName());
+		 * 
+		 * world.setBlockToAir(x, y, z); WorldServer worldServer = (WorldServer)
+		 * world;
+		 * 
+		 * 
+		 * 
+		 * LogHelper.info(oldBiome.biomeName + " >> " + newBiome.biomeName);
+		 * 
+		 * // update chunk's biome values Chunk chunk =
+		 * world.getChunkFromBlockCoords(x, z); byte[] chunkBiomes =
+		 * chunk.getBiomeArray(); for (int k = 0; k < chunkBiomes.length; ++k) {
+		 * chunkBiomes[k] = (byte) ( newBiome.biomeID & 0xFF ); }
+		 * chunk.setBiomeArray(chunkBiomes); chunk.needsSaving(true);
+		 * 
+		 * LogHelper.info("-1");
+		 * 
+		 * int chunkX = chunk.xPosition; int chunkZ = chunk.zPosition;
+		 * 
+		 * // TODO: move all players in the blast radius out of the way
+		 * List<EntityPlayerMP> players = Lists.newArrayList();
+		 * players.addAll(worldServer.playerEntities); for (EntityPlayerMP p :
+		 * players) { if(
+		 * worldServer.getPlayerManager().isPlayerWatchingChunk(p, chunkX,
+		 * chunkZ)) { LogHelper.info(" - pushing " + p);
+		 * p.setLocationAndAngles((chunkX - 5) << 4, p.posY, (chunkZ - 5) << 4,
+		 * 0F, 0F); worldServer.updateEntityWithOptionalForce(p, false); } }
+		 * 
+		 * LogHelper.info("0");
+		 * 
+		 * // unload affected chunk GenesisChunkProvider provider = new
+		 * GenesisChunkProvider(world, newBiome);
+		 * provider.unloadChunksIfNotNearSpawn(chunkX, chunkZ);
+		 * 
+		 * int loadCount = 0; while (provider.getLoadedChunkCount() !=
+		 * loadCount) { loadCount = provider.getLoadedChunkCount();
+		 * provider.unloadQueuedChunks(); if (chunk != null) { if(
+		 * chunk.isChunkLoaded ) { LogHelper.warning("Failed to unload chunk!");
+		 * loadCount = 0; } else { LogHelper.info("Chunk unloaded."); } chunk =
+		 * null; // better safe than sorry } }
+		 * 
+		 * LogHelper.info("1");
+		 * 
+		 * // regenerate the chunk IChunkLoader loader =
+		 * provider.getCurrentChunkLoader();
+		 * provider.setCurrentChunkLoader(null); provider.loadChunk(chunkX,
+		 * chunkZ); provider.populate(provider, chunkX, chunkZ);
+		 * provider.setCurrentChunkLoader(loader);
+		 * 
+		 * LogHelper.info("2");
+		 * 
+		 * PlayerInstance watcher = worldServer.getPlayerManager()
+		 * .getOrCreateChunkWatcher(chunkX, chunkZ, false); if (watcher != null)
+		 * { chunk = world.getChunkFromChunkCoords(chunkZ, chunkZ);
+		 * Packet51MapChunk packet = new Packet51MapChunk(chunk, true, -1);
+		 * watcher.sendToAllPlayersWatchingChunk(packet); } else {
+		 * LogHelper.warning("Unable to find chunk watcher!"); }
+		 * 
+		 * LogHelper.info("3");
+		 */
 	}
 
 }
