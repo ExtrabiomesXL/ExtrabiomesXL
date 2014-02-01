73b61cb7b3cb44356c496b14cb7168eaec4bd628
Ammon Lauritzen <allaryin@gmail.com>
2014-02-01 16:02:16 -0600

    New flower generator logic, more closely honors config for flower density.

--- src/main/java/extrabiomes/module/summa/worldgen/FlowerGenerator.java
+++ src/main/java/extrabiomes/module/summa/worldgen/FlowerGenerator.java
@@ -7,6 +7,7 @@
 package extrabiomes.module.summa.worldgen;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
@@ -15,6 +16,7 @@ import net.minecraft.world.biome.BiomeGenBase;
 import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraft.world.gen.feature.WorldGenerator;
 
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 
 import cpw.mods.fml.common.IWorldGenerator;
@@ -25,10 +27,152 @@ import extrabiomes.lib.BiomeSettings;
 public class FlowerGenerator implements IWorldGenerator
 {
 	private final Map<BlockType, WorldGenerator>	flowerGens;
+	private final Map<BiomeSettings, List<BlockCustomFlower.BlockType>>	flowerMaps;
     
     public FlowerGenerator()
     {
     	flowerGens = Maps.newHashMap();
+		flowerMaps = Maps.newHashMap();
+
+		registerFlower(BiomeSettings.ALPINE, BlockType.HYDRANGEA);
+		registerFlower(BiomeSettings.ALPINE, BlockType.IRIS_BLUE);
+		registerFlower(BiomeSettings.ALPINE, BlockType.IRIS_PURPLE);
+		registerFlower(BiomeSettings.ALPINE, BlockType.PANSY);
+		registerFlower(BiomeSettings.ALPINE, BlockType.VIOLET);
+
+		registerFlower(BiomeSettings.AUTUMNWOODS, BlockType.ALLIUM);
+		registerFlower(BiomeSettings.AUTUMNWOODS, BlockType.REDROVER);
+
+		registerFlower(BiomeSettings.BIRCHFOREST, BlockType.ALLIUM);
+		registerFlower(BiomeSettings.BIRCHFOREST, BlockType.BLUEBELL);
+		registerFlower(BiomeSettings.BIRCHFOREST, BlockType.BUTTERCUP);
+		registerFlower(BiomeSettings.BIRCHFOREST, BlockType.DAISY);
+		registerFlower(BiomeSettings.BIRCHFOREST, BlockType.DANDELION);
+		registerFlower(BiomeSettings.BIRCHFOREST, BlockType.HYDRANGEA);
+		registerFlower(BiomeSettings.BIRCHFOREST, BlockType.PANSY);
+		registerFlower(BiomeSettings.BIRCHFOREST, BlockType.REDROVER);
+		registerFlower(BiomeSettings.BIRCHFOREST, BlockType.TULIP);
+		registerFlower(BiomeSettings.BIRCHFOREST, BlockType.VIOLET);
+
+		registerFlower(BiomeSettings.EXTREMEJUNGLE, BlockType.GARDENIA);
+
+		registerFlower(BiomeSettings.FORESTEDHILLS, BlockType.ALLIUM);
+		registerFlower(BiomeSettings.FORESTEDHILLS, BlockType.BLUEBELL);
+		registerFlower(BiomeSettings.FORESTEDHILLS, BlockType.DAISY);
+		registerFlower(BiomeSettings.FORESTEDHILLS, BlockType.DANDELION);
+		registerFlower(BiomeSettings.FORESTEDHILLS, BlockType.HYDRANGEA);
+		registerFlower(BiomeSettings.FORESTEDHILLS, BlockType.IRIS_BLUE);
+		registerFlower(BiomeSettings.FORESTEDHILLS, BlockType.IRIS_PURPLE);
+		registerFlower(BiomeSettings.FORESTEDHILLS, BlockType.LAVENDER);
+		registerFlower(BiomeSettings.FORESTEDHILLS, BlockType.LILY);
+		registerFlower(BiomeSettings.FORESTEDHILLS, BlockType.ORIENTAL_PINK_LILY);
+		registerFlower(BiomeSettings.FORESTEDHILLS, BlockType.TULIP);
+		registerFlower(BiomeSettings.FORESTEDHILLS, BlockType.VIOLET);
+
+		registerFlower(BiomeSettings.FORESTEDISLAND, BlockType.ALLIUM);
+		registerFlower(BiomeSettings.FORESTEDISLAND, BlockType.DAISY);
+		registerFlower(BiomeSettings.FORESTEDISLAND, BlockType.DANDELION);
+		registerFlower(BiomeSettings.FORESTEDISLAND, BlockType.IRIS_BLUE);
+		registerFlower(BiomeSettings.FORESTEDISLAND, BlockType.IRIS_PURPLE);
+		registerFlower(BiomeSettings.FORESTEDISLAND, BlockType.LAVENDER);
+		registerFlower(BiomeSettings.FORESTEDISLAND, BlockType.LILY);
+		registerFlower(BiomeSettings.FORESTEDISLAND, BlockType.ORIENTAL_PINK_LILY);
+
+		registerFlower(BiomeSettings.GREENHILLS, BlockType.ALLIUM);
+		registerFlower(BiomeSettings.GREENHILLS, BlockType.BACHELORS_BUTTON);
+		registerFlower(BiomeSettings.GREENHILLS, BlockType.BLUEBELL);
+		registerFlower(BiomeSettings.GREENHILLS, BlockType.DAISY);
+		registerFlower(BiomeSettings.GREENHILLS, BlockType.DANDELION);
+		registerFlower(BiomeSettings.GREENHILLS, BlockType.IRIS_BLUE);
+		registerFlower(BiomeSettings.GREENHILLS, BlockType.IRIS_PURPLE);
+		registerFlower(BiomeSettings.GREENHILLS, BlockType.LILY);
+		registerFlower(BiomeSettings.GREENHILLS, BlockType.ORIENTAL_PINK_LILY);
+		registerFlower(BiomeSettings.GREENHILLS, BlockType.TULIP);
+		registerFlower(BiomeSettings.GREENHILLS, BlockType.VIOLET);
+
+		registerFlower(BiomeSettings.GREENSWAMP, BlockType.ALLIUM);
+		registerFlower(BiomeSettings.GREENSWAMP, BlockType.BUTTERCUP);
+		registerFlower(BiomeSettings.GREENSWAMP, BlockType.DAISY);
+		registerFlower(BiomeSettings.GREENSWAMP, BlockType.DANDELION);
+		registerFlower(BiomeSettings.GREENSWAMP, BlockType.EELGRASS);
+		registerFlower(BiomeSettings.GREENSWAMP, BlockType.LILY);
+		registerFlower(BiomeSettings.GREENSWAMP, BlockType.MARSH_MARIGOLD);
+		registerFlower(BiomeSettings.GREENSWAMP, BlockType.ORIENTAL_PINK_LILY);
+
+		registerFlower(BiomeSettings.MEADOW, BlockType.ALLIUM);
+		registerFlower(BiomeSettings.MEADOW, BlockType.BACHELORS_BUTTON);
+		registerFlower(BiomeSettings.MEADOW, BlockType.BUTTERCUP);
+		registerFlower(BiomeSettings.MEADOW, BlockType.DAISY);
+		registerFlower(BiomeSettings.MEADOW, BlockType.DANDELION);
+		registerFlower(BiomeSettings.MEADOW, BlockType.GERBERA_ORANGE);
+		registerFlower(BiomeSettings.MEADOW, BlockType.GERBERA_PINK);
+		registerFlower(BiomeSettings.MEADOW, BlockType.GERBERA_RED);
+		registerFlower(BiomeSettings.MEADOW, BlockType.GERBERA_YELLOW);
+		registerFlower(BiomeSettings.MEADOW, BlockType.HYDRANGEA);
+		registerFlower(BiomeSettings.MEADOW, BlockType.POPPY);
+		registerFlower(BiomeSettings.MEADOW, BlockType.YARROW);
+
+		registerFlower(BiomeSettings.MINIJUNGLE, BlockType.GARDENIA);
+
+		registerFlower(BiomeSettings.MOUNTAINTAIGA, BlockType.PANSY);
+
+		registerFlower(BiomeSettings.PINEFOREST, BlockType.BLUEBELL);
+		registerFlower(BiomeSettings.PINEFOREST, BlockType.BUTTERCUP);
+		registerFlower(BiomeSettings.PINEFOREST, BlockType.PANSY);
+		registerFlower(BiomeSettings.PINEFOREST, BlockType.REDROVER);
+		registerFlower(BiomeSettings.PINEFOREST, BlockType.TULIP);
+		registerFlower(BiomeSettings.PINEFOREST, BlockType.VIOLET);
+
+		registerFlower(BiomeSettings.RAINFOREST, BlockType.GARDENIA);
+
+		registerFlower(BiomeSettings.REDWOODLUSH, BlockType.BLUEBELL);
+		registerFlower(BiomeSettings.REDWOODLUSH, BlockType.TULIP);
+		registerFlower(BiomeSettings.REDWOODLUSH, BlockType.VIOLET);
+
+		registerFlower(BiomeSettings.SAVANNA, BlockType.ALLIUM);
+		registerFlower(BiomeSettings.SAVANNA, BlockType.AMARYLLIS_PINK);
+		registerFlower(BiomeSettings.SAVANNA, BlockType.AMARYLLIS_RED);
+		registerFlower(BiomeSettings.SAVANNA, BlockType.AMARYLLIS_WHITE);
+		registerFlower(BiomeSettings.SAVANNA, BlockType.BELLS_OF_IRELAND);
+		registerFlower(BiomeSettings.SAVANNA, BlockType.CALLA_BLACK);
+		registerFlower(BiomeSettings.SAVANNA, BlockType.CALLA_WHITE);
+		registerFlower(BiomeSettings.SAVANNA, BlockType.DAISY);
+		registerFlower(BiomeSettings.SAVANNA, BlockType.DANDELION);
+		registerFlower(BiomeSettings.SAVANNA, BlockType.GERBERA_ORANGE);
+		registerFlower(BiomeSettings.SAVANNA, BlockType.GERBERA_PINK);
+		registerFlower(BiomeSettings.SAVANNA, BlockType.GERBERA_RED);
+		registerFlower(BiomeSettings.SAVANNA, BlockType.GERBERA_YELLOW);
+		registerFlower(BiomeSettings.SAVANNA, BlockType.HYDRANGEA);
+		registerFlower(BiomeSettings.SAVANNA, BlockType.LAVENDER);
+		registerFlower(BiomeSettings.SAVANNA, BlockType.POPPY);
+		registerFlower(BiomeSettings.SAVANNA, BlockType.SNAPDRAGON);
+		registerFlower(BiomeSettings.SAVANNA, BlockType.YARROW);
+
+		registerFlower(BiomeSettings.SHRUBLAND, BlockType.ALLIUM);
+		registerFlower(BiomeSettings.SHRUBLAND, BlockType.BACHELORS_BUTTON);
+		registerFlower(BiomeSettings.SHRUBLAND, BlockType.BLUEBELL);
+		registerFlower(BiomeSettings.SHRUBLAND, BlockType.DAISY);
+		registerFlower(BiomeSettings.SHRUBLAND, BlockType.DANDELION);
+		registerFlower(BiomeSettings.SHRUBLAND, BlockType.GERBERA_ORANGE);
+		registerFlower(BiomeSettings.SHRUBLAND, BlockType.GERBERA_PINK);
+		registerFlower(BiomeSettings.SHRUBLAND, BlockType.GERBERA_RED);
+		registerFlower(BiomeSettings.SHRUBLAND, BlockType.GERBERA_YELLOW);
+		registerFlower(BiomeSettings.SHRUBLAND, BlockType.HYDRANGEA);
+		registerFlower(BiomeSettings.SHRUBLAND, BlockType.SNAPDRAGON);
+		registerFlower(BiomeSettings.SHRUBLAND, BlockType.TULIP);
+		registerFlower(BiomeSettings.SHRUBLAND, BlockType.YARROW);
+
+		registerFlower(BiomeSettings.WOODLANDS, BlockType.ALLIUM);
+		registerFlower(BiomeSettings.WOODLANDS, BlockType.BACHELORS_BUTTON);
+		registerFlower(BiomeSettings.WOODLANDS, BlockType.BLUEBELL);
+		registerFlower(BiomeSettings.WOODLANDS, BlockType.DAISY);
+		registerFlower(BiomeSettings.WOODLANDS, BlockType.DANDELION);
+		registerFlower(BiomeSettings.WOODLANDS, BlockType.IRIS_BLUE);
+		registerFlower(BiomeSettings.WOODLANDS, BlockType.IRIS_PURPLE);
+		registerFlower(BiomeSettings.WOODLANDS, BlockType.LAVENDER);
+		registerFlower(BiomeSettings.WOODLANDS, BlockType.LILY);
+		registerFlower(BiomeSettings.WOODLANDS, BlockType.ORIENTAL_PINK_LILY);
+		registerFlower(BiomeSettings.WOODLANDS, BlockType.TULIP);
     }
     
 	public void registerBlock(int blockID, Collection<BlockType> types) {
@@ -48,18 +192,43 @@ public class FlowerGenerator implements IWorldGenerator
 			flowerGens.put(type, gen);
 		}
 	}
+
+	protected void registerFlower(BiomeSettings settings, BlockType type) {
+		if (!settings.getBiome().isPresent()) return;
+		final List<BlockType> list;
+		if (flowerMaps.containsKey(settings)) {
+			list = flowerMaps.get(settings);
+		} else {
+			list = Lists.newArrayList();
+			flowerMaps.put(settings, list);
+		}
+		list.add(type);
+	}
     
-	protected void applyGenerator(BlockType type, World world, int chunkX,
+	protected int applyGenerator(BlockType type, World world, int chunkX,
 			int chunkZ, Random rand, int times) {
-		WorldGenerator gen = flowerGens.get(type);
+		final WorldGenerator gen = flowerGens.get(type);
+		int count = 0;
 		if (gen != null) {
 			for (int i = 0; i < times; ++i) {
 				final int x = chunkX + rand.nextInt(16) + 8;
 				final int y = rand.nextInt(128);
 				final int z = chunkZ + rand.nextInt(16) + 8;
-				gen.generate(world, rand, x, y, z);
+				if (gen.generate(world, rand, x, y, z)) ++count;
 			}
 		}
+		return count;
+	}
+
+	protected boolean applyGenerator(BlockType type, World world, int chunkX, int chunkZ, Random rand) {
+		final WorldGenerator gen = flowerGens.get(type);
+		if (gen != null) {
+			final int x = chunkX + rand.nextInt(16) + 8;
+			final int y = rand.nextInt(128);
+			final int z = chunkZ + rand.nextInt(16) + 8;
+			return gen.generate(world, rand, x, y, z);
+		}
+		return false;
 	}
 
 	protected boolean biomeCheck(BiomeSettings settings, BiomeGenBase biome) {
@@ -74,7 +243,48 @@ public class FlowerGenerator implements IWorldGenerator
         chunkZ = chunkZ << 4;
         final BiomeGenBase biome = world.getBiomeGenForCoords(chunkX, chunkZ);
         
-		if (biomeCheck(BiomeSettings.ALPINE, biome)) {
+		final BiomeSettings settings = BiomeSettings
+				.findBiomeSettings(biome.biomeID);
+		if (settings != null && biomeCheck(settings, biome) && flowerMaps.containsKey(settings)) {
+			final int maxFlowers = Math.max(biome.theBiomeDecorator.flowersPerChunk, 1);
+			List<BlockType> map = flowerMaps.get(settings);
+			for (int flowers = 0; flowers < maxFlowers; ++flowers) {
+				final int idx = rand.nextInt(map.size());
+				final BlockType type = map.get(idx);
+				applyGenerator(type, world, chunkX, chunkZ, rand);
+			}
+		}
+
+		// //////// nonstandard flowers //////////
+
+		if (biomeCheck(BiomeSettings.MOUNTAINDESERT, biome)) {
+			applyGenerator(BlockType.TINY_CACTUS, world, chunkX, chunkZ, rand, 70);
+		}
+
+		if (biomeCheck(BiomeSettings.MOUNTAINRIDGE, biome)) {
+			applyGenerator(BlockType.TINY_CACTUS, world, chunkX, chunkZ, rand, 62);
+		}
+
+		if (biomeCheck(BiomeSettings.AUTUMNWOODS, biome)
+				|| biomeCheck(BiomeSettings.SNOWYRAINFOREST, biome)
+				|| biomeCheck(BiomeSettings.TEMPORATERAINFOREST, biome)
+				|| biomeCheck(BiomeSettings.TUNDRA, biome)) {
+			applyGenerator(BlockType.TOADSTOOL, world, chunkX, chunkZ, rand, 2);
+		}
+    }
+    
+	/*
+	 * Old style generation method, creates _way_ too many flowers :)
+	 */
+	@SuppressWarnings("unused")
+	@Deprecated
+    private void _generate(Random rand, int chunkX, int chunkZ, World world, IChunkProvider chunkGenerator, IChunkProvider chunkProvider)
+ {
+	    chunkX = chunkX << 4;
+	    chunkZ = chunkZ << 4;
+	    final BiomeGenBase biome = world.getBiomeGenForCoords(chunkX, chunkZ);
+
+	    if (biomeCheck(BiomeSettings.ALPINE, biome)) {
 			applyGenerator(BlockType.HYDRANGEA, world, chunkX, chunkZ, rand, 1);
 			applyGenerator(BlockType.IRIS_BLUE, world, chunkX, chunkZ, rand, 1);
 			applyGenerator(BlockType.IRIS_PURPLE, world, chunkX, chunkZ, rand, 1);
