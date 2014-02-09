2066a38c52dab9159d2e3b610b3688b0b56c5f57
Ammon Lauritzen <allaryin@gmail.com>
2014-02-09 01:06:32 -0600

    Work on compressing crops into a single item id (#53).

--- src/main/java/extrabiomes/handlers/CropHandler.java
+++ src/main/java/extrabiomes/handlers/CropHandler.java
@@ -1,24 +1,48 @@
 package extrabiomes.handlers;
 
-import java.util.List;
-
-import com.google.common.collect.Lists;
-
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+
+import com.google.common.base.Optional;
+
+import extrabiomes.api.Stuff;
 import extrabiomes.blocks.BlockCropBasic;
 import extrabiomes.blocks.BlockCropRegrow;
 import extrabiomes.helpers.LogHelper;
+import extrabiomes.items.ItemCustomCrop;
 import extrabiomes.lib.BlockSettings;
 import extrabiomes.lib.Element;
 import extrabiomes.lib.ItemSettings;
 
 public class CropHandler {
     public static void createCrops() {
+		createCropItems();
+		createSeedItems();
     	createBasicCrops();
     	createRegrowCrops();
     }
     
+	private static void createCropItems() {
+		final ItemSettings settings = ItemSettings.CROP;
+		ItemCustomCrop item = new ItemCustomCrop(settings.getID());
+		Stuff.crop = Optional.of(item);
+
+		for (ItemCustomCrop.CropType type : ItemCustomCrop.CropType.values()) {
+			final Element element;
+			try {
+				element = Element.valueOf("CROP_" + type.name());
+			} catch( Exception e ) {
+				LogHelper.warning("Missing element for crop " + type);
+    			continue;
+			}
+
+			element.set(new ItemStack(item.itemID, 1, type.meta));
+		}
+	}
+
+	private static void createSeedItems() {
+		final ItemSettings settings = ItemSettings.SEED;
+	}
+
     private static void createBasicCrops() {
     	for( BlockCropBasic.CropType type : BlockCropBasic.CropType.values() ) {
     	}
@@ -27,34 +51,21 @@ public class CropHandler {
     private static void createRegrowCrops() {
     	for( BlockCropRegrow.CropType type : BlockCropRegrow.CropType.values() ) {
     		final BlockSettings plant_settings;
-    		final ItemSettings seed_settings;
-    		final ItemSettings crop_settings;
-    		
     		final Element plant_element;
-    		final Element seed_element;
-    		final Element crop_element;
     		try {
     			final String name = type.name();
     			
     			plant_settings = BlockSettings.valueOf(name);
-    			seed_settings = ItemSettings.valueOf("SEED_"+name);
-    			crop_settings = ItemSettings.valueOf("CROP_"+name);
-    			
-    			plant_element = Element.valueOf("PLANT_"+name);
-    			seed_element = Element.valueOf("SEED_"+name);
-    			crop_element = Element.valueOf("CROP_"+name);
+				plant_element = Element.valueOf("PLANT_" + name);
     		} catch( Exception e ) {
 				LogHelper.warning("Missing settings or elements for plant " + type);
     			continue;
     		}
     		
     		final BlockCropRegrow block = new BlockCropRegrow(plant_settings.getID(), type);
-    		final Item seed = new Item(seed_settings.getID());
-    		final Item crop = new Item(crop_settings.getID());
-    		
-    		plant_element.set(new ItemStack(block));
-    		seed_element.set(new ItemStack(seed));
-    		crop_element.set(new ItemStack(crop));
+			plant_element.set(new ItemStack(block));
+
+			// TODO: attach seeds and crops to blocks
     	}
     }
 
