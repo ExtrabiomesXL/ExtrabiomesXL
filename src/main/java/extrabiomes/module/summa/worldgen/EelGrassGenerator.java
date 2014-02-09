867bc099ca79eea23ff3acdadf3b499cc0345d38
JLBShecky <jlbshecky@gmail.com>
2014-02-09 04:59:18 -0500

    Eelgrass
    A prototype of the block system for mc 1.7/v4. This plant base has the
    possibility for configurable max height, although at the moment. As the
    code stands now plants can not grow similar to how the cactus or reads
    do, but they can be placed or generated as such.
    
    At the moment there is not to the plants but that could come later.
    
    Closes #37

---
+++ src/main/java/extrabiomes/module/summa/worldgen/EelGrassGenerator.java
@@ -0,0 +1,41 @@
+/**
+ * This work is licensed under the Creative Commons
+ * Attribution-ShareAlike 3.0 Unported License. To view a copy of this
+ * license, visit http://creativecommons.org/licenses/by-sa/3.0/.
+ */
+
+package extrabiomes.module.summa.worldgen;
+
+import java.util.Random;
+
+import net.minecraft.world.World;
+import net.minecraft.world.biome.BiomeGenBase;
+import net.minecraft.world.chunk.IChunkProvider;
+import net.minecraft.world.gen.feature.WorldGenerator;
+import cpw.mods.fml.common.IWorldGenerator;
+import extrabiomes.lib.BiomeSettings;
+
+public class EelGrassGenerator implements IWorldGenerator
+{
+    
+    private final WorldGenerator eelGrassGen;
+    
+    public EelGrassGenerator(int blockID, int metaData)
+    {
+        eelGrassGen = new WorldGenEelGrass(blockID, metaData);
+    }
+    
+    @Override
+    public void generate(Random rand, int chunkX, int chunkZ, World world, IChunkProvider chunkGenerator, IChunkProvider chunkProvider) {
+        chunkX = chunkX << 4;
+        chunkZ = chunkZ << 4;
+        final BiomeGenBase biome = world.getBiomeGenForCoords(chunkX, chunkX);
+        
+        for (int i = 0; i < 20; i++) {
+            final int x = chunkX + rand.nextInt(16) + 8;
+            final int z = chunkZ + rand.nextInt(16) + 8;
+            final int y = world.getHeightValue(x, z);
+            eelGrassGen.generate(world, rand, x, y, z);
+        }
+    }
+}
---
+++ src/main/java/extrabiomes/module/summa/worldgen/WorldGenEelGrass.java
@@ -0,0 +1,50 @@
+/**
+ * This work is licensed under the Creative Commons
+ * Attribution-ShareAlike 3.0 Unported License. To view a copy of this
+ * license, visit http://creativecommons.org/licenses/by-sa/3.0/.
+ */
+
+package extrabiomes.module.summa.worldgen;
+
+import java.util.Random;
+
+import extrabiomes.helpers.LogHelper;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.world.World;
+import net.minecraft.world.gen.feature.WorldGenerator;
+
+class WorldGenEelGrass extends WorldGenerator
+{
+    
+    private final int blockID;
+    private final int metaData;
+    
+    WorldGenEelGrass(int blockID, int metaData) {
+        this.blockID = blockID;
+        this.metaData = metaData;
+    }
+    
+    @Override
+    public boolean generate(World world, Random rand, int x, int y, int z) {
+        int i;
+        int maxDepth = 4;
+        while ((Block.blocksList[i = world.getBlockId(x, y, z)] == null) && y > 0) {
+            y--;
+        }
+        
+        while(((i = world.getBlockId(x, y, z)) == Block.waterMoving.blockID || i == Block.waterStill.blockID) && y > 0 && maxDepth > 0) {
+        	y--;
+        	maxDepth--;
+        }
+        
+        y++;
+        
+        if (Block.blocksList[blockID].canBlockStay(world, x, y, z)) {
+        	world.setBlock(x, y, z, blockID);
+        } 
+
+        return true;
+    }
+    
+}
