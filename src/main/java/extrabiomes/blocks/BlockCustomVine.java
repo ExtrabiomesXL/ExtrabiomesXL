03c230c21b219214ba69fe1c490e7c8515eb8d0c
JLBShecky <jlbshecky@gmail.com>
2014-02-13 12:53:16 -0500

    Gloriosa Vile Color
    The Gloriosa Vine now renders in the same independent of the biome the
    block is located in presently.

--- src/main/java/extrabiomes/blocks/BlockCustomVine.java
+++ src/main/java/extrabiomes/blocks/BlockCustomVine.java
@@ -5,6 +5,8 @@ import cpw.mods.fml.relauncher.SideOnly;
 import extrabiomes.Extrabiomes;
 import net.minecraft.block.BlockVine;
 import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.world.ColorizerFoliage;
+import net.minecraft.world.IBlockAccess;
 
 public class BlockCustomVine extends BlockVine {
 
@@ -24,6 +26,20 @@ public class BlockCustomVine extends BlockVine {
     {
     	final String iconPath = Extrabiomes.TEXTURE_PATH + "vine_" + type.toString().toLowerCase();
 		this.blockIcon = iconRegister.registerIcon(iconPath);
+    }
+
+    /*
+    @SideOnly(Side.CLIENT)
+    public int getBlockColor()
+    {
+        return 0xffffff;
+    }//*/
+    
+    @SideOnly(Side.CLIENT)
+
+    public int colorMultiplier(IBlockAccess par1IBlockAccess, int par2, int par3, int par4)
+    {
+        return 0xffffff;
     }
 
 }
