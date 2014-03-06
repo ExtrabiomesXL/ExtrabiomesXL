440d9370b0ca639486aafcb6532c4040dd90af77
JLBShecky <jlbshecky@gmail.com>
2014-03-05 23:42:55 -0500

    Update build Numbers

--- src/main/java/extrabiomes/lib/Reference.java
+++ src/main/java/extrabiomes/lib/Reference.java
@@ -9,10 +9,10 @@ package extrabiomes.lib;
 public abstract class Reference
 {
     // Mod information
-    public static final String MOD_ID       	= "ExtrabiomesXL";
-    public static final String MOD_NAME     	= MOD_ID;
-	public static final String MOD_VERSION		= "3.15.6";
-    public static final String CONFIG_VERSION 	= "3.15"; // only update on config file changes
-    public static final String CLIENT_PROXY 	= "extrabiomes.proxy.ClientProxy";
-    public static final String SERVER_PROXY 	= "extrabiomes.proxy.CommonProxy";
+    public static final String MOD_ID           = "ExtrabiomesXL";
+    public static final String MOD_NAME         = MOD_ID;
+    public static final String MOD_VERSION      = "3.15.7";
+    public static final String CONFIG_VERSION   = "3.15"; // only update on config file changes
+    public static final String CLIENT_PROXY     = "extrabiomes.proxy.ClientProxy";
+    public static final String SERVER_PROXY     = "extrabiomes.proxy.CommonProxy";
 }
