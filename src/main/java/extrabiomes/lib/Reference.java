d261b32adbd37c956b7743a8087d2a16cd3369dd
Ammon Lauritzen <allaryin@gmail.com>
2014-08-01 16:43:34 -0500

    Set version number for 3.16 alpha.

--- src/main/java/extrabiomes/lib/Reference.java
+++ src/main/java/extrabiomes/lib/Reference.java
@@ -11,8 +11,8 @@ public abstract class Reference
     // Mod information
     public static final String MOD_ID           = "extrabiomesxl";
     public static final String MOD_NAME         = "ExtrabiomesXL";
-	public static final String MOD_VERSION		= "3.15.8";
-    public static final String CONFIG_VERSION   = "3.15"; // only update on config file changes
+	public static final String MOD_VERSION		= "3.16.alpha;
+    public static final String CONFIG_VERSION   = "3.16"; // only update on config file changes
     public static final String CLIENT_PROXY     = "extrabiomes.proxy.ClientProxy";
     public static final String SERVER_PROXY     = "extrabiomes.proxy.CommonProxy";
 }
