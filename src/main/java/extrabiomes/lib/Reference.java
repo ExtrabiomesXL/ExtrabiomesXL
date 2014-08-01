4cf0eafe99a99992100bf4ca55391b12376375a7
Ammon Lauritzen <allaryin@gmail.com>
2014-08-01 16:51:12 -0500

    Al can typo good.

--- src/main/java/extrabiomes/lib/Reference.java
+++ src/main/java/extrabiomes/lib/Reference.java
@@ -11,7 +11,7 @@ public abstract class Reference
     // Mod information
     public static final String MOD_ID           = "extrabiomesxl";
     public static final String MOD_NAME         = "ExtrabiomesXL";
-	public static final String MOD_VERSION		= "3.16.alpha;
+	public static final String MOD_VERSION		= "3.16.alpha";
     public static final String CONFIG_VERSION   = "3.16"; // only update on config file changes
     public static final String CLIENT_PROXY     = "extrabiomes.proxy.ClientProxy";
     public static final String SERVER_PROXY     = "extrabiomes.proxy.CommonProxy";
